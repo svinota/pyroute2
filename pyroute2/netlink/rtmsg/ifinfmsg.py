830ae0bc2004dcb85b201dd0b89b69e01e6e772c
Peter V. Saveliev <peet@redhat.com>
2013-04-09 14:38:33 +0200

    rtnl: update ifinfmsg
    + IFLA_STATS
    + IFLA_STATS64
    + IFLA_GROUP
    + IFLA_PROMISCUITY
    + IFLA_NUM_TX_QUEUES
    + IFLA_NUM_RX_QUEUES

--- pyroute2/netlink/rtmsg/ifinfmsg.py
+++ pyroute2/netlink/rtmsg/ifinfmsg.py
@@ -26,7 +26,28 @@ class t_ifmap(nlmsg):
     the attribute mapping, since nlmsg supports this API.
     """
     fmt = "QQQHBB"
-    fiels = ("mem_start", "mem_end", "base_addr", "irq", "dma", "port")
+    fields = ("mem_start", "mem_end", "base_addr", "irq", "dma", "port")
+
+
+class t_ifstats(nlmsg):
+    """
+    Interface statistics
+    """
+    fmt = "IIIIIIIIIIIIIIIIIIIIIII"
+    fields = ("rx_packets", "tx_packets", "rx_bytes", "tx_bytes",
+              "rx_errors", "tx_errors", "rx_dropped", "tx_dropped",
+              "multicast", "collisions", "rx_length_errors", "rx_over_errors",
+              "rx_crc_errors", "rx_frame_errors", "rx_fifo_errors",
+              "rx_missed_errors", "tx_aborted_errors", "tx_carrier_errors",
+              "tx_fifo_errors", "tx_heartbeat_errors", "tx_window_errors",
+              "rx_compressed", "tx_compressed")
+
+
+class t_ifstats64(t_ifstats):
+    """
+    Interface statistics, 64bit version
+    """
+    fmt = "QQQQQQQQQQQQQQQQQQQQQQQ"
 
 
 ## link attributes
@@ -48,6 +69,21 @@ IFLA_MAP = 14
 IFLA_WEIGHT = 15
 IFLA_OPERSTATE = 16
 IFLA_LINKMODE = 17
+IFLA_LINKINFO = 18
+IFLA_NET_NS_PID = 19
+IFLA_IFALIAS = 20
+IFLA_NUM_VF = 21
+IFLA_VFINFO_LIST = 22
+IFLA_STATS64 = 23
+IFLA_VF_PORTS = 24
+IFLA_PORT_SELF = 25
+IFLA_AF_SPEC = 26
+IFLA_GROUP = 27
+IFLA_NET_NS_FD = 28
+IFLA_EXT_MASK = 29
+IFLA_PROMISCUITY = 30
+IFLA_NUM_TX_QUEUES = 31
+IFLA_NUM_RX_QUEUES = 32
 
 IF_OPER_UNKNOWN = 0
 IF_OPER_NOTPRESENT = 1
@@ -60,18 +96,23 @@ IF_OPER_UP = 6
 (IF_OPER_NAMES, IF_OPER_VALUES) = map_namespace("IF_OPER", globals())
 
 
-t_ifla_attr = {IFLA_UNSPEC:    (t_none,        "none"),
-               IFLA_ADDRESS:   (t_l2ad,        "hwaddr"),
-               IFLA_BROADCAST: (t_l2ad,        "broadcast"),
-               IFLA_IFNAME:    (t_asciiz,      "dev"),
-               IFLA_MTU:       (t_uint32,      "mtu"),
-               IFLA_LINK:      (t_uint32,      "link"),
-               IFLA_QDISC:     (t_asciiz,      "qdisc"),
-               IFLA_STATS:     (t_none,        "stats"),
-               IFLA_OPERSTATE: (t_state,       "state"),
-               IFLA_TXQLEN:    (t_uint32,      "txqlen"),
-               IFLA_LINKMODE:  (t_uint8,       "linkmode"),
-               IFLA_MAP:       (t_ifmap,       "ifmap")}
+t_ifla_attr = {IFLA_UNSPEC:         (t_none,        "none"),
+               IFLA_ADDRESS:        (t_l2ad,        "hwaddr"),
+               IFLA_BROADCAST:      (t_l2ad,        "broadcast"),
+               IFLA_IFNAME:         (t_asciiz,      "dev"),
+               IFLA_MTU:            (t_uint32,      "mtu"),
+               IFLA_LINK:           (t_uint32,      "link"),
+               IFLA_QDISC:          (t_asciiz,      "qdisc"),
+               IFLA_STATS:          (t_ifstats,     "stats"),
+               IFLA_STATS64:        (t_ifstats64,   "stats64"),
+               IFLA_OPERSTATE:      (t_state,       "state"),
+               IFLA_TXQLEN:         (t_uint32,      "txqlen"),
+               IFLA_LINKMODE:       (t_uint8,       "linkmode"),
+               IFLA_MAP:            (t_ifmap,       "ifmap"),
+               IFLA_GROUP:          (t_uint32,      "group"),
+               IFLA_PROMISCUITY:    (t_uint32,      "promiscuity"),
+               IFLA_NUM_TX_QUEUES:  (t_uint32,      "tx queues"),
+               IFLA_NUM_RX_QUEUES:  (t_uint32,      "rx queues")}
 
 
 class ifinfmsg(nlmsg):
