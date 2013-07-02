b1e643b44259fd298f923f23915140b89393bc2a
Peter V. Saveliev <peet@redhat.com>
2013-07-02 17:37:46 +0200

    tests: ingress filters and sfq tests

--- tests/test_tc.py
+++ tests/test_tc.py
@@ -1,9 +1,9 @@
 import socket
 from utils import require_user
-from utils import grep
 from pyroute2 import IPRoute
 from pyroute2.netlink.iproute import RTM_NEWQDISC
 from pyroute2.netlink.iproute import RTM_NEWTFILTER
+from pyroute2.netlink.iproute import TC_H_INGRESS
 
 
 class BasicTest(object):
@@ -27,12 +27,14 @@ class TestIngress(BasicTest):
     def test_simple(self):
         self.ip.tc(RTM_NEWQDISC, 'ingress', self.interface, 0xffff0000)
         # get qdiscs list and filter out our interface
-        qds = [x for x in self.ip.get_qdiscs() if
-               x['index'] == self.interface]
+        qds = [x for x in self.ip.get_qdiscs() if x['index'] == self.interface]
         # assert the list is not empty
         assert qds
         # assert there is the ingress queue
-        assert qds[0].get_attr('TCA_KIND') == ['ingress']
+        assert qds[0].get_attr('TCA_KIND') == 'ingress'
+        # assert it has proper handle and parent
+        assert qds[0]['handle'] == 0xffff0000
+        assert qds[0]['parent'] == TC_H_INGRESS
 
     def test_filter(self):
         self.test_simple()
@@ -45,5 +47,29 @@ class TestIngress(BasicTest):
                    limit=0,
                    prio=50,
                    keys=['0x0/0x0+12'])
-        assert grep('tc filter show dev dummyX parent ffff:',
-                    'rate 10000bit burst 10Kb')
+        fls = self.ip.get_filters(index=self.interface, parent=0xffff0000)
+        # assert there are filters
+        assert fls
+        # assert there is one police rule:
+        prs = [x for x in fls
+               if x.get_attr('TCA_OPTIONS') is not None and
+               x.get_attr('TCA_OPTIONS').get_attr('TCA_U32_POLICE')
+               is not None][0]
+        # assert the police rule has specified parameters
+        options = prs.get_attr('TCA_OPTIONS')
+        police_u32 = options.get_attr('TCA_U32_POLICE')
+        police_tbf = police_u32.get_attr('TCA_POLICE_TBF')
+        assert police_tbf['rate'] == 1250
+        assert police_tbf['mtu'] == 2040
+
+
+class TestSfq(BasicTest):
+
+    def test_sfq(self):
+        self.ip.tc(RTM_NEWQDISC, 'sfq', self.interface, 0, perturb=10)
+        qds = [x for x in self.ip.get_qdiscs() if x['index'] == self.interface]
+        if qds:
+            qds = qds[0]
+        assert qds
+        assert qds.get_attr('TCA_KIND') == 'sfq'
+        assert qds.get_attr('TCA_OPTIONS')['perturb_period'] == 10
