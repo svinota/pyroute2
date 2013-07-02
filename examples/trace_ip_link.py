c54a7f4820fdb314a80f57d183a337fa36ecd3a6
Peter V. Saveliev <peet@redhat.com>
2013-07-02 17:38:23 +0200

    examples: trace arbitrary command from command line

--- examples/trace_ip_link.py
+++ examples/trace_ip_link.py
@@ -4,13 +4,13 @@ Launch external command 'ip link show' and analyze
 netlink traffic, that it sends/receives. Just like
 tcpdump, but for netlink protocol.
 '''
-
+import sys
 from pyroute2 import IPRoute
 from Queue import Empty
 from pprint import pprint
 
 ip = IPRoute()
-q = ip.connect('ptrace://ip link show')
+q = ip.connect('ptrace://' + ' '.join(sys.argv[1:]))
 while True:
     try:
         pprint(ip.get(q, raw=True))
