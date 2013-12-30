0ae01b565230a3df9dc45b49b76a8f9b0f710d74
Peter V. Saveliev <peter@svinota.eu>
2013-12-30 13:49:00 +0100

    iocore: NLT_DGRAM flag fix
    + examples update

--- examples/push_client.py
+++ examples/push_client.py
@@ -1,7 +1,9 @@
 from pyroute2 import IOCore
+from pyroute2.iocore import NLT_DGRAM
 
 ioc = IOCore()
 (uid, addr) = ioc.connect('tcp://localhost:9824')
-port = ioc.discover(addr, 'push')
-ioc.push(addr, port, 'hello, world!')
+ioc.connect('udp://localhost:9824')
+port = ioc.discover('push', addr)
+ioc.push((addr, port), 'hello, world!', NLT_DGRAM)
 ioc.release()
--- examples/push_server.py
+++ examples/push_server.py
@@ -2,6 +2,7 @@ from pyroute2 import IOCore
 
 ioc = IOCore(addr=0x02000000)
 ioc.serve('tcp://0.0.0.0:9824')
+ioc.serve('udp://0.0.0.0:9824')
 ioc.provide('push')
 
 print('waiting message from client')
