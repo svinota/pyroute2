d0c1f6a0cc6972b88dfd91c5487e724186b98ed3
Peter V. Saveliev <peter@svinota.eu>
2014-02-05 11:26:30 +0100

    examples: merge push/pull

--- examples/push_client.py
+++
@@ -1,9 +0,0 @@
-from pyroute2 import IOCore
-from pyroute2.iocore import NLT_DGRAM
-
-ioc = IOCore()
-(uid, addr) = ioc.connect('tcp://localhost:9824')
-ioc.connect('udp://localhost:9824')
-port = ioc.discover('push', addr)
-ioc.push((addr, port), 'hello, world!', NLT_DGRAM)
-ioc.release()
