1b4c9d0084455716e5d6d4cd198280281064e141
Peter V. Saveliev <peter@svinota.eu>
2014-02-07 13:11:31 +0100

    tests: avoid duplicate connection
    FIXME!

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -37,27 +37,26 @@ class Namespace(object):
 class TestPush(object):
 
     def setup(self):
-        url_tcp = 'tcp://0.0.0.0:9823/push'
-        url_udp = 'udp://0.0.0.0:9823/push'
+        self.url_tcp = 'tcp://0.0.0.0:9823/push'
+        self.url_udp = 'udp://0.0.0.0:9823/push'
         self.node1 = Node()
-        self.node1.serve(url_tcp)
-        self.node1.serve(url_udp)
+        self.node1.serve(self.url_tcp)
+        self.node1.serve(self.url_udp)
         self.node1.mirror()
-
         self.node2 = Node()
-        self.proxy_tcp = self.node2.target(url_tcp)
-        self.proxy_udp = self.node2.target(url_tcp, url_udp, NLT_DGRAM)
 
     def teardown(self):
         self.node2.shutdown()
         self.node1.shutdown()
 
     def test_tcp_push(self):
-        self.proxy_tcp.push('test1')
+        proxy = self.node2.target(self.url_tcp)
+        proxy.push('test1')
         assert self.node1.get() == b'test1'
 
     def test_udp_push(self):
-        self.proxy_udp.push('test2')
+        proxy = self.node2.target(self.url_tcp, self.url_udp, NLT_DGRAM)
+        proxy.push('test2')
         assert self.node1.get() == b'test2'
 
 
