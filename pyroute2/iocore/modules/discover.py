ebaef47875d7e36cc39815f00ff3ffe9c2e5a981
Peter V. Saveliev <peter@svinota.eu>
2014-03-28 22:35:47 +0000

    iocore: discovery of wrong service -> exception
    A try to discover non-existent service should raise a
    proper exception

--- pyroute2/iocore/modules/discover.py
+++ pyroute2/iocore/modules/discover.py
@@ -9,5 +9,4 @@ level = access.ANY
 def command(broker, sock, env, cmd, rsp):
     # .. _ioc-discover:
     url = cmd.get_attr('IPR_ATTR_HOST')
-    if url in broker.discover:
-        rsp['attrs'].append(['IPR_ATTR_ADDR', broker.discover[url]])
+    rsp['attrs'].append(['IPR_ATTR_ADDR', broker.discover[url]])
