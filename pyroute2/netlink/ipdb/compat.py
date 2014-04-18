3a2efcf00f6d7fc36955007cf09a1300d80c7275
Peter V. Saveliev <peter@svinota.eu>
2014-04-18 21:39:48 +0200

    compat: use decorators for compat functions
    That complicates overall logic, but simplifies and speeds up runtime.

--- pyroute2/netlink/ipdb/compat.py
+++ pyroute2/netlink/ipdb/compat.py
@@ -15,79 +15,110 @@ _ANCIENT_PLATFORM = (platform.dist()[0] in ('redhat', 'centos') and
                      platform.dist()[1].startswith('6.'))
 
 
+#
+#
+#
+def bypass(f):
+    if _ANCIENT_PLATFORM:
+        return f
+    else:
+        def _bypass(*argv, **kwarg):
+            pass
+        return _bypass
+
+
+def replace(r):
+    def decorator(f):
+        if _ANCIENT_PLATFORM:
+            return f
+        else:
+            return r
+    return decorator
+
+
+@bypass
 def fix_del_master(port):
-    if _ANCIENT_PLATFORM and 'master' in port:
+    if 'master' in port:
         port.del_item('master')
 
 
+@bypass
 def fix_add_master(port, master):
-    if _ANCIENT_PLATFORM and 'master' not in port:
+    if 'master' not in port:
         port.set_item('master', master['index'])
 
 
+@bypass
 def fix_check_link(nl, index):
-    if _ANCIENT_PLATFORM:
-        # swith mirror off
-        nl.mirror(False)
-        # check, if the link really exits --
-        # on some old kernels you can receive
-        # broadcast RTM_NEWLINK after the link
-        # was deleted
-        try:
-            nl.get_links(index)
-        except NetlinkError as e:
-            if e.code == 19:  # No such device
-                # just drop this message then
-                return True
-        finally:
-            nl.mirror(True)
+    # swith mirror off
+    nl.mirror(False)
+    # check, if the link really exits --
+    # on some old kernels you can receive
+    # broadcast RTM_NEWLINK after the link
+    # was deleted
+    try:
+        nl.get_links(index)
+    except NetlinkError as e:
+        if e.code == 19:  # No such device
+            # just drop this message then
+            return True
+    finally:
+        nl.mirror(True)
 
 
+@bypass
 def fix_lookup_master(interface):
-    if _ANCIENT_PLATFORM:
-        master = get_master(interface['ifname'])
-        interface.set_item('master', master)
-        return master
+    master = get_master(interface['ifname'])
+    interface.set_item('master', master)
+    return master
 
 
+@replace(lambda n, l: n.link('delete', index=l['index']))
 def fix_del_link(nl, link):
-    if _ANCIENT_PLATFORM and link['kind'] == 'bridge':
+    if link['kind'] == 'bridge':
         del_bridge(link['ifname'])
-    elif _ANCIENT_PLATFORM and link['kind'] == 'bond':
+    elif link['kind'] == 'bond':
         del_bond(link['ifname'])
     else:
         nl.link('delete', index=link['index'])
 
 
+@replace(lambda n, m, p: n.link('set', index=p['index'], master=m['index']))
 def fix_add_port(nl, master, port):
-    if _ANCIENT_PLATFORM and master['kind'] == 'bridge':
+    if master['kind'] == 'bridge':
         add_bridge_port(master['ifname'], port['ifname'])
-    elif _ANCIENT_PLATFORM and master['kind'] == 'bond':
+    elif master['kind'] == 'bond':
         add_bond_port(master['ifname'], port['ifname'])
     else:
         nl.link('set', index=port['index'], master=master['index'])
 
 
+@replace(lambda n, m, p: n.link('set', index=p['index'], master=0))
 def fix_del_port(nl, master, port):
-    if _ANCIENT_PLATFORM and master['kind'] == 'bridge':
+    if master['kind'] == 'bridge':
         del_bridge_port(master['ifname'], port['ifname'])
-    elif _ANCIENT_PLATFORM and master['kind'] == 'bond':
+    elif master['kind'] == 'bond':
         del_bond_port(master['ifname'], port['ifname'])
     else:
         nl.link('set', index=port['index'], master=0)
 
 
+@replace(lambda n, l: n.link('add', **l))
 def fix_create_link(nl, link):
     # transparently support ancient platforms
-    if _ANCIENT_PLATFORM and link['kind'] == 'bridge':
+    if link['kind'] == 'bridge':
         create_bridge(link['ifname'])
-    elif _ANCIENT_PLATFORM and link['kind'] == 'bond':
+    elif link['kind'] == 'bond':
         create_bond(link['ifname'])
     else:
         # the normal case for any modern kernel
         nl.link('add', **link)
 
 
+#
+# Utility functions to call external programs or set up
+# network objects w/o RT netlink, e.g. via sysfs
+#
 def get_master(name):
     f = None
 
