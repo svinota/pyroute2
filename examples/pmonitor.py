eb336bdbf13655bc2d8fcb28d01d34b1976edfa2
Peter V. Saveliev <peter@svinota.eu>
2014-05-23 16:29:17 +0200

    examples: read number of cpus from /proc/cpuinfo

--- examples/pmonitor.py
+++ examples/pmonitor.py
@@ -4,7 +4,13 @@ Monitor process exit
 from pyroute2 import TaskStats
 from pyroute2.common import hexdump
 
-pmask = "1"
+pmask = ''
+
+with open('/proc/cpuinfo', 'r') as f:
+    for line in f.readlines():
+        if line.startswith('processor'):
+            pmask += ',' + line.split()[2]
+pmask = pmask[1:]
 ts = TaskStats()
 ts.register_mask(pmask)
 msg = ts.get()[0]
