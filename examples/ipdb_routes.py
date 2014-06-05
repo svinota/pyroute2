254701d71cc774851b4caa862e6aedbf17d1374b
Peter V. Saveliev <peter@svinota.eu>
2014-06-05 06:03:31 +0000

    examples: fix ipdb routes example
    Use dummy device to host routes

--- examples/ipdb_routes.py
+++ examples/ipdb_routes.py
@@ -2,17 +2,25 @@ from pyroute2 import IPDB
 
 ip = IPDB()
 
+# create dummy interface to host routes on
+ip.create(kind='dummy', ifname='pr2test').\
+    add_ip('172.16.1.1/24').\
+    up().\
+    commit()
+
 # create a route
 with ip.routes.add({'dst': '172.16.0.0/24',
-                    'gateway': '192.168.122.2'}) as r:
+                    'gateway': '172.16.1.2'}) as r:
     pass
 
 # modify it
 with ip.routes['172.16.0.0/24'] as r:
-    r.gateway = '192.168.122.1'
+    r.gateway = '172.16.1.3'
 
-# remove
+# cleanup
 with ip.routes['172.16.0.0/24'] as r:
     r.remove()
+
+ip.interfaces.pr2test.remove().commit()
 
 ip.release()
