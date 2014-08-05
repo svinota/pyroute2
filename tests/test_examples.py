f8b5970f4b28c5a7f067aa1e6c4f447943595eed
Peter V. Saveliev <peter@svinota.eu>
2014-08-05 12:10:55 +0200

    tests: revert increased timeout in test_examples launcher

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -34,7 +34,7 @@ class TestExamples(object):
                                      stdin=subprocess.PIPE,
                                      stdout=subprocess.PIPE,
                                      stderr=fnull)
-                time.sleep(3)
+                time.sleep(1)
             if importlib is not None:
                 importlib.import_module(client)
             else:
