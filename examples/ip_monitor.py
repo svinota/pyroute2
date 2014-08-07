3b16ebbb6257b72b6110c17c5fd433e92bf0200f
Peter V. Saveliev <peter@svinota.eu>
2014-08-07 11:43:22 +0200

    examples: add ip_monitor

---
+++ examples/ip_monitor.py
@@ -0,0 +1,10 @@
+'''
+Simplest example to monitor Netlink events with a Python script.
+'''
+
+from pyroute2 import IPRSocket
+from pprint import pprint
+
+ip = IPRSocket()
+ip.bind()
+pprint(ip.get())
