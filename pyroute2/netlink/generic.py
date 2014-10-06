6d9f53ca73fb71fd78c19f61608a5a5e8985d705
Peter V. Saveliev <peter@svinota.eu>
2014-10-06 16:56:14 +0200

    generic.py: deprecated, use the new layout

--- pyroute2/netlink/generic.py
+++
@@ -1,9 +0,0 @@
-import logging
-from pyroute2.netlink import nlmsg
-from pyroute2.netlink import genlmsg
-
-logging.warning("Usage of pyroute2.netlink.generic "
-                "is deprecated, see "
-                "http://pyroute2.org/docs/migration.html")
-mofules = [nlmsg, genlmsg]
-__all__ = ["nlmsg", "genlmsg"]
