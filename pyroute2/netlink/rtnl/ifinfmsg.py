svinota/pyroute2

commit 4144c0012a39e4866ce561c51d5b9d7fe4a3aca6

rtnl: add veth struct support
Bug-Url: https://github.com/svinota/pyroute2/issues/84

pyroute2/netlink/rtnl/ifinfmsg.py
@@ -76,7 +76,7 @@ stats_names = ('rx_packets',
                'tx_compressed')
 
 
-class ifinfmsg(nlmsg):
+class ifinfbase(object):
     '''
     Network interface message
     struct ifinfomsg {
@@ -207,7 +207,16 @@ class ifinfmsg(nlmsg):
                 return self.vlan_data
             elif kind == 'bond':
                 return self.bond_data
+            elif kind == 'veth':
+                return self.veth_data
             return self.hex
+
+        class veth_data(nla):
+            nla_map = (('VETH_INFO_UNSPEC', 'none'),
+                       ('VETH_INFO_PEER', 'info_peer'))
+
+            def info_peer(self, *argv, **kwarg):
+                return ifinfveth
 
         class vlan_data(nla):
             nla_map = (('IFLA_VLAN_UNSPEC', 'none'),
@@ -393,3 +402,11 @@ class ifinfmsg(nlmsg):
                           ('outmsgs', 'Q'),
                           ('outerrors', 'Q'),
                           ('inmsgs', 'Q'))
+
+
+class ifinfmsg(ifinfbase, nlmsg):
+    pass
+
+
+class ifinfveth(ifinfbase, nla):
+    pass

commit c09dcb3b8abddfdaf6969e93cf335ce39f128faf

rtnl: fix ifinfmsg encoding
bug introduced with 4144c0012a39e4866ce561c51d5b9d7fe4a3aca6

pyroute2/netlink/rtnl/ifinfmsg.py
@@ -157,7 +157,7 @@ class ifinfbase(object):
         # convert flags
         if isinstance(self['flags'], (set, tuple, list)):
             self['flags'], self['change'] = self.names2flags(self['flags'])
-        return nlmsg.encode(self)
+        return super(ifinfbase, self).encode()
 
     class wireless(iw_event):
         pass