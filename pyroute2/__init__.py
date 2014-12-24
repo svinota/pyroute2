9b82fd3ad9fdcdecad18cb5f266498cdf0a054c8
Peter V. Saveliev <petr.savelyev@ericsson.com>
2014-12-24 17:51:15 +0100

    netns: re-export from the root library namespace

--- pyroute2/__init__.py
+++ pyroute2/__init__.py
@@ -1,6 +1,7 @@
 
 from pyroute2.iproute import IPRoute
 from pyroute2.ipdb import IPDB
+from pyroute2.netns import NetNS
 from pyroute2.netlink.rtnl import IPRSocket
 from pyroute2.netlink.taskstats import TaskStats
 from pyroute2.netlink.ipq import IPQSocket
@@ -10,6 +11,7 @@ from pyroute2.netlink import NetlinkError
 modules = [IPRSocket,
            IPRoute,
            IPDB,
+           NetNS,
            TaskStats,
            IPQSocket,
            GenericNetlinkSocket,
