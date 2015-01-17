2789ca1f4566b0541f2d3b7550c74c338d4fd1b6
Peter V. Saveliev <peter@svinota.eu>
2015-01-17 11:57:28 +0100

    taskstats: fix the example

--- examples/taskstats.py
+++ examples/taskstats.py
@@ -7,6 +7,8 @@ from pyroute2.common import hexdump
 
 pid = os.getpid()
 ts = TaskStats()
+# bind is required in the case of generic netlink
+ts.bind()
 ret = ts.get_pid_stat(int(pid))[0]
 # raw hex structure to check alignment
 print(hexdump(ret.raw))
