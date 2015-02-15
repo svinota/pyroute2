86d418e5a2b452f6ae7f1200b8b33695949a21dd
Peter V. Saveliev <peter@svinota.eu>
2015-02-15 22:52:21 +0100

    examples: remove outdated comment
    from `custom_socket_base`

--- examples/custom_socket_base/custom_socket_base.py
+++ examples/custom_socket_base/custom_socket_base.py
@@ -2,14 +2,9 @@
 #
 # This example shows how to define and use a custom socket base
 # class to be used with NetlinkSocket.
-
 #
 # socket_wrapper module overrides the SocketBase; only after
 # that we should import IPRoute
-#
-# In order to make this work, one should switch pyroute2
-# autoimports off with the environment variable, see run.sh
-# script for details
 #
 # Override SocketBase
 import socket_wrapper
@@ -17,7 +12,6 @@ import socket_wrapper
 from pyroute2.iproute import IPRoute
 # make PEP8 happy
 __all__ = ["socket_wrapper", "IPRoute"]
-
 
 ip = IPRoute()
 print(ip.get_addr())
