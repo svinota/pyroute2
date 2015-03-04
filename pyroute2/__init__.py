19e02bfaa13131b2b912df7d5ec03721a4f83868
Peter V. Saveliev <peter@svinota.eu>
2015-03-04 15:41:26 +0100

    common: provide basic docs in the root proxy
    There are two ways to import modules from pyroute2.
    
    * `from pyroute2 import <symbol>`
    or
    * `from pyroute2.full.path import <symbol>`
    
    The first way allows do not rely on the module layout: the layout can change,
    but "root" imports will stay the same. `from pyroute2 import IPRoute` will
    always be `from pyroute2 import IPRoute`.
    
    Also, "root" imports do not provide the required class immediately. Instead
    they provide a proxy, that loads module only on demand. That's not about
    the startup speed, but it allows to safely replace some base classes via
    `pyroute2.config`, and only after that load the module.
    
    It is not a monkey-patching, though looks like that.
    
    This patch provides not only proxies, but also a basic docstring to the proxies.
    Please note, that referring the docstring by itself also causes the module
    load.

--- pyroute2/__init__.py
+++ pyroute2/__init__.py
@@ -1,14 +1,15 @@
 ##
-# defer all root imports
+# Defer all root imports
 #
-# this allows to safely import config, change it, and
-# only after that import modules itself
+# This allows to safely import config, change it, and
+# only after that actually run imports, though the
+# import statement can be on the top of the file
 #
-# surely, you can also import modules directly from their
-# places
+# Viva PEP8, morituri te salutant!
+#
+# Surely, you still can import modules directly from their
+# places, like `from pyroute2.iproute import IPRoute`
 ##
-from functools import partial
-
 __all__ = []
 _modules = {'IPRoute': 'pyroute2.iproute',
             'IPDB': 'pyroute2.ipdb',
@@ -21,13 +22,71 @@ _modules = {'IPRoute': 'pyroute2.iproute',
             'GenericNetlinkSocket': 'pyroute2.netlink.generic',
             'NetlinkError': 'pyroute2.netlink'}
 
+_DISCLAIMER = '''\n\nNotice:\n
+This is a proxy class. To read full docs, please run
+the `help()` method on the instance instead.
 
-def _wrapper(name, *argv, **kwarg):
-    _temp = __import__(_modules[name], globals(), locals(), [name], 0)
-    return getattr(_temp, name)(*argv, **kwarg)
+Usage of the proxy allows to postpone the module load,
+thus providing a safe way to substitute base classes,
+if it is required. More details see in the `pyroute2.config`
+module.
+\n'''
+
+
+class _ProxyMeta(type):
+    '''
+    All this metaclass alchemy is implemented to provide a
+    reasonable, though not exhaustive documentation on the
+    proxy classes.
+    '''
+
+    def __init__(cls, name, bases, dct):
+
+        class doc(str):
+            def __repr__(self):
+                return repr(cls.proxy['doc'])
+
+            def __str__(self):
+                return str(cls.proxy['doc'])
+
+            def expandtabs(self, ts=4):
+                return cls.proxy['doc'].expandtabs(ts)
+
+        class proxy(object):
+            def __init__(self):
+                self.target = {}
+
+            def __getitem__(self, key):
+                if not self.target:
+                    module = __import__(_modules[cls.name],
+                                        globals(),
+                                        locals(),
+                                        [cls.name], 0)
+                    self.target['constructor'] = getattr(module, cls.name)
+                    self.target['doc'] = self.target['constructor'].__doc__
+                    try:
+                        self.target['doc'] += _DISCLAIMER
+                    except TypeError:
+                        # ignore cases, when __doc__ is not a string, e.g. None
+                        pass
+                return self.target[key]
+
+        def __call__(self, *argv, **kwarg):
+            '''
+            Actually load the module and call the constructor.
+            '''
+            return self.proxy['constructor'](*argv, **kwarg)
+
+        cls.name = name
+        cls.proxy = proxy()
+        cls.__call__ = __call__
+        cls.__doc__ = doc()
+
+        super(_ProxyMeta, cls).__init__(name, bases, dct)
+
 
 for name in _modules:
-    f = partial(_wrapper, name)
-    f.__name__ = name
+
+    f = _ProxyMeta(name, (), {})()
     globals()[name] = f
     __all__.append(name)
