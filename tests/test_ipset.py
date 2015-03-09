ddab4b060a377f9d2767e1e5cb088fb4d2b31a11
Peter V. Saveliev <peter@svinota.eu>
2015-03-09 17:41:20 +0100

    tests: cover ipset `add()`/`delete()`

--- tests/test_ipset.py
+++ tests/test_ipset.py
@@ -11,11 +11,22 @@ class TestIPSet(object):
     def teardown(self):
         self.ip.close()
 
+    def list_ipset(self, name):
+        try:
+            return [x.get_attr('IPSET_ATTR_IP_FROM').
+                    get_attr('IPSET_ATTR_IPADDR_IPV4')
+                    for x in
+                    self.ip.list(name)[0].
+                    get_attr('IPSET_ATTR_ADT').
+                    get_attrs('IPSET_ATTR_PROTO')]
+        except:
+            return []
+
     def get_ipset(self, name):
         return [x for x in self.ip.list()
                 if x.get_attr('IPSET_ATTR_SETNAME') == name]
 
-    def test_create_remove(self):
+    def test_create_destroy(self):
         require_user('root')
         name = str(uuid4())[:16]
         # create ipset
@@ -23,6 +34,25 @@ class TestIPSet(object):
         # assert it exists
         assert self.get_ipset(name)
         # remove ipset
-        self.ip.delete(name)
+        self.ip.destroy(name)
         # assert it is removed
+        assert not self.get_ipset(name)
+
+    def test_add_delete(self):
+        require_user('root')
+        name = str(uuid4())[:16]
+        ipaddr = '192.168.1.1'
+        # create ipset
+        self.ip.create(name)
+        assert self.get_ipset(name)
+        # add an entry
+        self.ip.add(name, ipaddr)
+        # check it
+        assert ipaddr in self.list_ipset(name)
+        # delete an entry
+        self.ip.delete(name, ipaddr)
+        # check it
+        assert ipaddr not in self.list_ipset(name)
+        # remove ipset
+        self.ip.destroy(name)
         assert not self.get_ipset(name)
