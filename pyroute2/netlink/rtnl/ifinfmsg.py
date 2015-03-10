6d445479759d26819334fa50925f31ded8673483
Peter V. Saveliev <peter@svinota.eu>
2015-03-10 13:08:24 +0100

    vxlan: correct fields order
    Bug-Url: https://github.com/svinota/pyroute2/issues/112

--- pyroute2/netlink/rtnl/ifinfmsg.py
+++ pyroute2/netlink/rtnl/ifinfmsg.py
@@ -338,21 +338,28 @@ class ifinfbase(object):
             nla_map = (('IFLA_VXLAN_UNSPEC', 'none'),
                        ('IFLA_VXLAN_ID', 'uint32'),
                        ('IFLA_VXLAN_GROUP', 'ip4addr'),
-                       ('IFLA_VXLAN_GROUP6', 'hex'),
                        ('IFLA_VXLAN_LINK', 'uint32'),
-                       ('IFLA_VXLAN_LOCAL', 'hex'),
-                       ('IFLA_VXLAN_LOCAL6', 'hex'),
-                       ('IFLA_VXLAN_TOS', 'uint8'),
+                       ('IFLA_VXLAN_LOCAL', 'ip4addr'),
                        ('IFLA_VXLAN_TTL', 'uint8'),
+                       ('IFLA_VXLAN_TOS', 'uint8'),
                        ('IFLA_VXLAN_LEARNING', 'uint8'),
                        ('IFLA_VXLAN_AGEING', 'uint32'),
                        ('IFLA_VXLAN_LIMIT', 'uint32'),
-                       ('IFLA_VXLAN_PORT_RANGE', 'hex'),
+                       ('IFLA_VXLAN_PORT_RANGE', 'port_range'),
                        ('IFLA_VXLAN_PROXY', 'uint8'),
                        ('IFLA_VXLAN_RSC', 'uint8'),
                        ('IFLA_VXLAN_L2MISS', 'uint8'),
                        ('IFLA_VXLAN_L3MISS', 'uint8'),
-                       ('IFLA_VXLAN_PORT', 'uint16'))
+                       ('IFLA_VXLAN_PORT', 'uint16'),
+                       ('IFLA_VXLAN_GROUP6', 'ip6addr'),
+                       ('IFLA_VXLAN_LOCAL6', 'ip6addr'),
+                       ('IFLA_VXLAN_UDP_CSUM', 'uint8'),
+                       ('IFLA_VXLAN_UDP_ZERO_CSUM6_TX', 'uint8'),
+                       ('IFLA_VXLAN_UDP_ZERO_CSUM6_RX', 'uint8'))
+
+            class port_range(nla):
+                fields = (('low', '>H'),
+                          ('high', '>H'))
 
         class vlan_data(nla):
             nla_map = (('IFLA_VLAN_UNSPEC', 'none'),
