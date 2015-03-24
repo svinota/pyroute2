dc8b85204936aa0a14c4bf1127c877d3b7121c81
Peter V. Saveliev <peter@svinota.eu>
2015-03-24 15:09:44 +0100

    dhcp: minimal working dhcp request code
    some values are hardcoded yet

--- pyroute2/dhcp/__init__.py
+++ pyroute2/dhcp/__init__.py
@@ -1,3 +1,4 @@
+import array
 import struct
 from socket import socket
 from socket import htons
@@ -12,6 +13,7 @@ from socket import SO_BROADCAST
 from socket import SO_REUSEADDR
 from socket import SO_BINDTODEVICE
 from pyroute2 import IPRoute
+from pyroute2.common import basestring
 
 ETH_P_ALL = 3
 
@@ -36,10 +38,12 @@ class msg(dict):
                          'encode': lambda x: [int(i, 16) for i in
                                               x.split(':')] + [10 * '\x00']}}
 
-    def __init__(self, content=None, buf=b''):
+    def __init__(self, content=None, buf=b'', offset=0, value=None):
         content = content or {}
         dict.__init__(self, content)
         self.buf = buf
+        self.offset = 0
+        self.value = value
         self._register_fields()
 
     def _register_fields(self):
@@ -53,30 +57,39 @@ class msg(dict):
         else:
             return (fmt, lambda x: [x])
 
+    def reset(self):
+        self.buf = b''
+
     def decode(self):
-        offset = 0
         self._register_fields()
         for field in self.fields:
             name, fmt = field[:2]
             fmt, routine = self._get_routine('decode', fmt)
             size = struct.calcsize(fmt)
-            value = struct.unpack(fmt, self.buf[offset:offset + size])[0]
+            value = struct.unpack(fmt, self.buf[self.offset:
+                                                self.offset + size])[0]
             self[name] = routine(value)
-            offset += size
+            self.offset += size
         return self
 
     def encode(self):
-        self.buf = b''
         self._register_fields()
         for field in self.fields:
             name, fmt = field[:2]
             default = '\x00' if len(field) <= 2 else field[2]
             fmt, routine = self._get_routine('encode', fmt)
-            size = struct.calcsize(fmt)
-            if self[name] is None:
-                self.buf += default * size
+            # special case: string
+            if fmt == 'string':
+                self.buf += routine(self[name])[0]
             else:
-                self.buf += struct.pack(fmt, *routine(self[name]))
+                size = struct.calcsize(fmt)
+                if self[name] is None:
+                    if not isinstance(default, basestring):
+                        self.buf += struct.pack(fmt, default)
+                    else:
+                        self.buf += default * (size / len(default))
+                else:
+                    self.buf += struct.pack(fmt, *routine(self[name]))
         return self
 
     def __getitem__(self, key):
@@ -86,6 +99,146 @@ class msg(dict):
             if key in self._fields_names:
                 return None
             raise
+
+
+class option(msg):
+
+    code = 0
+    policy = None
+    value = None
+
+    def __init__(self, content=None, buf=b'', offset=0, value=None, code=0):
+        msg.__init__(self, content=content, buf=buf,
+                     offset=offset, value=value)
+        self.code = code
+
+    @property
+    def length(self):
+        data_length = self.data_length
+        if data_length == 0:
+            return 1
+        else:
+            return data_length + 2
+
+    @property
+    def data_length(self):
+        s = 0
+        for field in self.fields:
+            fmt, routine = self._get_routine('decode', field[1])
+            if fmt == 'string':
+                s += len(self[field[0]])
+            else:
+                s += struct.calcsize(fmt)
+        if s == 0 and self.policy is not None:
+            if self.policy['fmt'] == 'string':
+                s = len(self.value)
+            else:
+                s = struct.calcsize(self.policy['fmt'])
+        return s
+
+    def encode(self):
+        # pack code
+        self.buf += struct.pack('B', self.code)
+        if self.code in (0, 255):
+            return self
+        self.buf += struct.pack('B', self.data_length)
+        if self.policy is not None:
+            value = self.policy.get('encode', lambda x: x)(self.value)
+            if self.policy['fmt'] == 'string':
+                fmt = '%is' % len(value)
+            else:
+                fmt = self.policy['fmt']
+            self.buf += struct.pack(fmt, value)
+        else:
+            msg.encode(self)
+        return self
+
+
+class dhcpmsg(msg):
+    options = ()
+    l2addr = None
+
+    def _register_options(self):
+        for option in self.options:
+            code, name, fmt = option[:3]
+            self._decode_map[code] =\
+                self._encode_map[name] = {'name': name,
+                                          'code': code,
+                                          'fmt': fmt}
+
+    def decode(self):
+        msg.decode(self)
+        while self.offset < len(self.buf):
+            code = struct.unpack('B', self.buf[self.offset:self.offset + 1])
+            # code is unknown -- bypass it
+            if code not in self._decode_map:
+                length = struct.unpack('B', self.buf[self.offset + 1:
+                                                     self.offset + 2])
+                self.offset += length + 2
+                continue
+
+            # code is known, work on it
+            option_class = getattr(self, self._decode_map[code]['fmt'])
+            option = option_class(buf=self.buf, offset=self.offset + 2)
+            option.decode()
+            self.offset += option.length
+            self['options'][self._decode_map[code]['name']] = option
+        return self
+
+    def encode(self):
+        msg.encode(self)
+        # put message type
+        self.buf += self.uint8(code=53, value=self.mtype).encode().buf
+        self.buf += self.client_id({'type': 1,
+                                    'key': self['chaddr']},
+                                   code=61).encode().buf
+        self.buf += self.string(code=60, value='pyroute2').encode().buf
+
+        for (name, value) in self.get('options', {}):
+            fmt = self._encode_map.get(name, {'fmt': None})['fmt']
+            if fmt is None:
+                continue
+            # name is known, ok
+            option_class = getattr(self, fmt)
+            if isinstance(value, dict):
+                option = option_class(value,
+                                      code=self._encode_map[name]['code'])
+            else:
+                option = option_class(code=self._encode_map[name]['code'],
+                                      value=value)
+            self.buf += option.encode().buf
+
+        self.buf += self.none(code=255).encode().buf
+        return self
+
+    class none(option):
+        pass
+
+    class be16(option):
+        policy = {'fmt': '>H'}
+
+    class be32(option):
+        policy = {'fmt': '>I'}
+
+    class uint8(option):
+        policy = {'fmt': 'B'}
+
+    class string(option):
+        policy = {'fmt': 'string'}
+
+    class array8(option):
+        policy = {'fmt': 'string',
+                  'encode': lambda x: array.array('B', x).tostring(),
+                  'decode': lambda x: array.array('B', x).tolist()}
+
+    class ip4addr(option):
+        policy = {'fmt': '4s',
+                  'encode': lambda x: inet_pton(AF_INET, x),
+                  'decode': lambda x: inet_ntop(AF_INET, x)}
+
+    class client_id(option):
+        fields = (('type', 'uint8'),
+                  ('key', 'l2addr'))
 
 
 class ethmsg(msg):
@@ -122,7 +275,7 @@ class udpmsg(msg):
               ('csum', 'be16'))
 
 
-class dhcp4msg(msg):
+class dhcp4msg(dhcpmsg):
     #
     # https://www.ietf.org/rfc/rfc2131.txt
     #
@@ -139,7 +292,27 @@ class dhcp4msg(msg):
               ('giaddr', 'ip4addr'),
               ('chaddr', 'l2paddr'),
               ('sname', '64s'),
-              ('file', '128s'))
+              ('file', '128s'),
+              ('cookie', '4s', 'c\x82Sc'))
+    #
+    # https://www.ietf.org/rfc/rfc2132.txt
+    #
+    options = ((0, 'pad', 'none'),
+               (1, 'subnet_mask', 'ip4addr'),
+               (2, 'time_offset', 'be32'),
+               (3, 'router', 'ip4addr'),
+               (4, 'time_server', 'ip4addr'),
+               (5, 'ien_name_server', 'ip4addr'),
+               (6, 'name_server', 'ip4addr'),
+               (7, 'log_server', 'ip4addr'),
+               (8, 'cookie_server', 'ip4addr'),
+               (9, 'lpr_server', 'ip4addr'),
+               (53, 'message_type', 'uint8'),
+               (55, 'parameter_list', 'array8'),
+               (57, 'messagi_size', 'be16'),
+               (60, 'vendor_id', 'string'),
+               (61, 'client_id', 'client_id'),
+               (255, 'end', 'none'))
 
 
 class DHCP4Socket(socket):
@@ -185,6 +358,7 @@ class DHCP4Socket(socket):
                       'proto': 17,
                       'dst': '255.255.255.255'})
         ip4['csum'] = self.csum(ip4.encode().buf)
+        ip4.reset()
         udp = udpmsg({'sport': 68,
                       'dport': 67,
                       'len': 8 + len(data)})
@@ -192,17 +366,20 @@ class DHCP4Socket(socket):
                                    'proto': 17,
                                    'len': 8 + len(data)})
         udp['csum'] = self.csum(udph.encode().buf + udp.encode().buf + data)
+        udp.reset()
         data = eth.encode().buf +\
             ip4.encode().buf +\
             udp.encode().buf +\
             data
         self.raw.send(data)
 
-    def put(self, msg=None, addr='255.255.255.255', port=67):
+    def put(self, options=None, msg=None, addr='255.255.255.255', port=67):
+        options = options or {}
         msg = msg or dhcp4msg({'op': 1,
                                'htype': 1,
                                'hlen': 6,
-                               'chaddr': self.l2addr})
+                               'chaddr': self.l2addr,
+                               'options': options})
 
         # fill required fields
         if msg['op'] is None:
@@ -214,6 +391,7 @@ class DHCP4Socket(socket):
             msg['chaddr'] = self.l2addr
         if msg['xid'] is None:
             msg['xid'] = 15
+        msg.mtype = 1
         msg.encode()
         self.send_raw(msg.buf)
 
