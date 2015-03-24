46f1d100a3b1095a07966ead42d134d792212718
Peter V. Saveliev <peter@svinota.eu>
2015-03-24 18:49:10 +0100

    dhcp: fix example
    Take ifname from command line

--- examples/dhclient.py
+++ examples/dhclient.py
@@ -1,6 +1,12 @@
+import sys
 from pprint import pprint
 from pyroute2.dhcp import DHCP4Socket
-s = DHCP4Socket('dhcp-if2')
+
+if len(sys.argv) > 1:
+    iface = sys.argv[1]
+else:
+    iface = 'eth0'
+s = DHCP4Socket(iface)
 s.put()
 print("DHCP response:\n")
 pprint(s.get())
