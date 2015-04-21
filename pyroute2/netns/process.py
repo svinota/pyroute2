d0ea6b3d24ff215c1a1b9ccd1781f144f73120c1
Peter V. Saveliev <peter@svinota.eu>
2015-04-21 22:38:04 +0200

    netns: improved `Popen` proxy
    Support methods and literal attributes.
    Support autocomplete in ipython.
    
    Bug-Url: https://github.com/svinota/pyroute2/issues/90

--- pyroute2/netns/process.py
+++ pyroute2/netns/process.py
@@ -1,8 +1,18 @@
+import types
 import atexit
 import subprocess
 from pyroute2.netns import setns
 from pyroute2.config import MpQueue
 from pyroute2.config import MpProcess
+
+
+def _handle(result):
+    if result['code'] == 500:
+        raise result['data']
+    elif result['code'] == 200:
+        return result['data']
+    else:
+        raise TypeError('unsupported return code')
 
 
 def NSPopenServer(nsname, flags, channel_in, channel_out, argv, kwarg):
@@ -12,23 +22,29 @@ def NSPopenServer(nsname, flags, channel_in, channel_out, argv, kwarg):
     except Exception as e:
         channel_out.put(e)
         return
-    channel_out.put(None)
     # create the Popen object
-    p = subprocess.Popen(*argv, **kwarg)
+    child = subprocess.Popen(*argv, **kwarg)
+    # send the API map
+    channel_out.put([(x, isinstance(getattr(child, x), types.MethodType))
+                     for x in dir(child)])
     while True:
         # synchronous mode
         # 1. get the command from the API
         call = channel_in.get()
         # 2. stop?
         if call['name'] == 'close':
-            return
+            break
         # 3. run the call
         try:
-            method = getattr(p, call['name'])
-            result = method(*call['argv'], **call['kwarg'])
+            attr = getattr(child, call['name'])
+            if isinstance(attr, types.MethodType):
+                result = attr(*call['argv'], **call['kwarg'])
+            else:
+                result = attr
             channel_out.put({'code': 200, 'data': result})
         except Exception as e:
             channel_out.put({'code': 500, 'data': e})
+    child.wait()
 
 
 class NSPopen(object):
@@ -50,30 +66,32 @@ class NSPopen(object):
                                       argv, kwarg))
         # start the child and check the status
         self.server.start()
-        error = self.channel_in.get()
-        if error is not None:
+        response = self.channel_in.get()
+        if isinstance(response, Exception):
             self.server.join()
-            raise error
+            raise response
         else:
+            self.api = dict(response)
             atexit.register(self.close)
 
     def close(self):
         self.channel_out.put({'name': 'close'})
+
+    def __dir__(self):
+        return self.api.keys()
 
     def __getattribute__(self, key):
         try:
             return object.__getattribute__(self, key)
         except AttributeError:
 
-            def proxy(*argv, **kwarg):
-                self.channel_out.put({'name': key,
-                                      'argv': argv,
-                                      'kwarg': kwarg})
-                result = self.channel_in.get()
-                if result['code'] == 500:
-                    raise result['data']
-                elif result['code'] == 200:
-                    return result['data']
-                else:
-                    raise TypeError('unsupported return code')
-            return proxy
+            if self.api.get(key):
+                def proxy(*argv, **kwarg):
+                    self.channel_out.put({'name': key,
+                                          'argv': argv,
+                                          'kwarg': kwarg})
+                    return _handle(self.channel_in.get())
+                return proxy
+            else:
+                self.channel_out.put({'name': key})
+                return _handle(self.channel_in.get())
