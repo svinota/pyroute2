bd6e349d3195b51c9a9576a66d03af90e7c87384
Peter V. Saveliev <peter@svinota.eu>
2015-04-26 00:49:40 +0200

    tests: cover new `AddrPool` public methods

--- tests/test_addrpool.py
+++ tests/test_addrpool.py
@@ -52,3 +52,52 @@ class TestAddrPool(object):
             ap.free(0)
         except KeyError:
             pass
+
+    def test_locate(self):
+
+        ap = AddrPool()
+        f = ap.alloc()
+        base1, bit1, is_allocated1 = ap.locate(f)
+        base2, bit2, is_allocated2 = ap.locate(f + 1)
+
+        assert base1 == base2
+        assert bit2 == bit1 + 1
+        assert is_allocated1
+        assert not is_allocated2
+        assert ap.allocated == 1
+
+    def test_setaddr_allocated(self):
+
+        ap = AddrPool()
+        f = ap.alloc()
+        base, bit, is_allocated = ap.locate(f + 1)
+        assert not is_allocated
+        assert ap.allocated == 1
+        ap.setaddr(f + 1, 'allocated')
+        base, bit, is_allocated = ap.locate(f + 1)
+        assert is_allocated
+        assert ap.allocated == 2
+        ap.free(f + 1)
+        base, bit, is_allocated = ap.locate(f + 1)
+        assert not is_allocated
+        assert ap.allocated == 1
+
+    def test_setaddr_free(self):
+
+        ap = AddrPool()
+        f = ap.alloc()
+        base, bit, is_allocated = ap.locate(f + 1)
+        assert not is_allocated
+        assert ap.allocated == 1
+        ap.setaddr(f + 1, 'free')
+        base, bit, is_allocated = ap.locate(f + 1)
+        assert not is_allocated
+        assert ap.allocated == 1
+        ap.setaddr(f, 'free')
+        base, bit, is_allocated = ap.locate(f)
+        assert not is_allocated
+        assert ap.allocated == 0
+        try:
+            ap.free(f)
+        except KeyError:
+            pass
