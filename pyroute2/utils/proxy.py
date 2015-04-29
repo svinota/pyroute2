49c215a25ead78cde32e8efd4ab2ede22a9d0090
Peter V. Saveliev <peter@svinota.eu>
2015-04-29 18:07:22 +0200

    debugger: turn script into module

--- pyroute2/utils/proxy.py
+++ pyroute2/utils/proxy.py
@@ -3,35 +3,44 @@ import socket
 import struct
 from pyroute2 import IPRoute
 
-nat = {}
-clients = []
 
-srv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-srv.bind(('0.0.0.0', 3546))
-ipr = IPRoute()
-ipr.bind()
+class Server(object):
 
-poll = select.poll()
-poll.register(ipr, select.POLLIN | select.POLLPRI)
-poll.register(srv, select.POLLIN | select.POLLPRI)
+    def __init__(self, addr='0.0.0.0', port=3546):
+        self.addr = addr
+        self.port = port
 
-while True:
-    events = poll.poll()
-    for (fd, event) in events:
-        if fd == ipr.fileno():
-            bufsize = ipr.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF) // 2
-            data = ipr.recv(bufsize)
-            cookie = struct.unpack('I', data[8:12])[0]
-            if cookie == 0:
-                for address in clients:
-                    srv.sendto(data, (address, 3547))
-            else:
-                srv.sendto(data, (nat[cookie], 3547))
-        else:
-            data, (address, port) = srv.recvfrom(16384)
-            if data is None:
-                clients.remove(address)
-                continue
-            cookie = struct.unpack('I', data[8:12])[0]
-            nat[cookie] = address
-            ipr.sendto(data, (0, 0))
+    def run(self):
+        nat = {}
+        clients = []
+
+        srv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        srv.bind((self.addr, self.port))
+        ipr = IPRoute()
+        ipr.bind()
+
+        poll = select.poll()
+        poll.register(ipr, select.POLLIN | select.POLLPRI)
+        poll.register(srv, select.POLLIN | select.POLLPRI)
+
+        while True:
+            events = poll.poll()
+            for (fd, event) in events:
+                if fd == ipr.fileno():
+                    bufsize = ipr.getsockopt(socket.SOL_SOCKET,
+                                             socket.SO_RCVBUF) // 2
+                    data = ipr.recv(bufsize)
+                    cookie = struct.unpack('I', data[8:12])[0]
+                    if cookie == 0:
+                        for address in clients:
+                            srv.sendto(data, address)
+                    else:
+                        srv.sendto(data, nat[cookie])
+                else:
+                    data, address = srv.recvfrom(16384)
+                    if data is None:
+                        clients.remove(address)
+                        continue
+                    cookie = struct.unpack('I', data[8:12])[0]
+                    nat[cookie] = address
+                    ipr.sendto(data, (0, 0))
