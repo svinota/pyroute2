4037c8122bee7642230104207f80e0899cbaf306
Peter V. Saveliev <peter@svinota.eu>
2015-04-29 18:09:49 +0200

    debugger: merge modules

--- pyroute2/debugger.py
+++ pyroute2/debugger.py
@@ -1,7 +1,12 @@
 import select
 import socket
 import struct
-from pyroute2 import IPRoute
+import threading
+from pyroute2.iproute import IPRoute
+try:
+    from Queue import Queue
+except ImportError:
+    from queue import Queue
 
 
 class Server(object):
@@ -44,3 +49,36 @@ class Server(object):
                     cookie = struct.unpack('I', data[8:12])[0]
                     nat[cookie] = address
                     ipr.sendto(data, (0, 0))
+
+
+class Client(IPRoute):
+
+    def __init__(self, addr):
+        IPRoute.__init__(self)
+        self.proxy = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.proxy.bind(('0.0.0.0', 3547))
+        self.proxy_addr = addr
+        self.proxy_queue = Queue()
+
+        def recv():
+            while True:
+                (data, addr) = self.proxy.recvfrom(16384)
+                self.proxy_queue.put(data)
+
+        self.pthread = threading.Thread(target=recv)
+        self.pthread.setDaemon(True)
+        self.pthread.start()
+
+        def sendto(buf, *argv, **kwarg):
+            return self.proxy.sendto(buf, (self.proxy_addr, 3546))
+
+        def recv(*argv, **kwarg):
+            return self.proxy_queue.get()
+
+        self._sendto = sendto
+        self._recv = recv
+
+    def close(self):
+        self.proxy.close()
+        self.recv = lambda *x, **y: None
+        self.proxy_queue.put(None)
