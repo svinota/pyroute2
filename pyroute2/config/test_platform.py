fe7a38add165e9a68ea9514cb129f4bf51d12a36
Peter V. Saveliev <peter@svinota.eu>
2015-05-21 21:05:11 +0200

    config: zombie_newlink platform test

--- pyroute2/config/test_platform.py
+++ pyroute2/config/test_platform.py
@@ -1,6 +1,7 @@
 '''
 A set of platform tests to discover the system capabilities
 '''
+import threading
 from pyroute2.common import uifname
 from pyroute2 import RawIPRoute
 
@@ -41,8 +42,62 @@ class TestCapsRtnl(object):
                     self.set_capability(name[5:], False)
                 self.teardown()
 
+    def test_create_dummy(self):
+        self.ip.link_create(ifname=self.ifnames[0], kind='dummy')
+
     def test_create_bridge(self):
         self.ip.link_create(ifname=self.ifnames[0], kind='bridge')
 
     def test_create_bond(self):
         self.ip.link_create(ifname=self.ifnames[0], kind='bond')
+
+    def test_zombie_newlink_dummy(self):
+        counters = {}
+        # bouncer try
+        #
+        # if the kernel doesn't support dummy interfaces, the
+        # test will fail immediately and will not start the
+        # monitoring thread
+        #
+        # this RTM_NEWLINK is not counted
+        self.ip.link_create(ifname=self.ifnames[0], kind='dummy')
+        #
+        # start monitoring thread
+        def monitor(counters):
+            ip = RawIPRoute()
+            ip.bind()
+            while counters.get('RTM_DELLINK', 0) < 2:
+                msgs = ip.get()
+                for msg in msgs:
+                    if msg.get_attr('IFLA_IFNAME') != self.ifnames[0]:
+                        # in an ideal case we should match indices
+                        continue
+                    if msg.get('event', None) in counters:
+                        counters[msg.get('event', None)] += 1
+                    else:
+                        counters[msg.get('event', None)] = 1
+            ip.close()
+        t = threading.Thread(target=monitor, args=(counters, ))
+        t.start()
+        # 1st delete
+        self.ip.link_remove(self.ip.link_lookup(ifname=self.ifnames[0]))
+        # 2nd create + delete, loops exits
+        self.ip.link_create(ifname=self.ifnames[0], kind='dummy')
+        self.ip.link_remove(self.ip.link_lookup(ifname=self.ifnames[0]))
+        # join the monitoring thread
+        t.join()
+        # assert counters
+        #
+        # normal flow:
+        #   RTM_DELLINK: 2
+        #   RTM_NEWLINK: 1
+        #
+        # the zombie case:
+        #   RTM_DELLINK: 2
+        #   RTM_NEWLINK: >= 2
+        #
+        # the cause is that when you delete an interface on old kernels,
+        # you get first RTM_DELLINK and then one or more RTM_NEWLINK for
+        # the same interface immediately
+        #
+        assert counters['RTM_NEWLINK'] > 1
