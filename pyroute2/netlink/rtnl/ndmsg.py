713762379ad18bb222454965ab8bc15af489abcb
Peter V. Saveliev <petr.savelyev@ericsson.com>
2015-12-01 22:09:15 +0100

    ndmsg: add NUD states
    Bug-Url: https://github.com/svinota/pyroute2/issues/198

--- pyroute2/netlink/rtnl/ndmsg.py
+++ pyroute2/netlink/rtnl/ndmsg.py
@@ -1,6 +1,22 @@
-
+from pyroute2.common import map_namespace
 from pyroute2.netlink import nlmsg
 from pyroute2.netlink import nla
+
+
+NUD_INCOMPLETE = 0x01
+NUD_REACHABLE = 0x02
+NUD_STALE = 0x04
+NUD_DELAY = 0x08
+NUD_PROBE = 0x10
+NUD_FAILED = 0x20
+
+# dummy states
+
+NUD_NOARP = 0x40
+NUD_PERMANENT = 0x80
+NUD_NONE = 0x00
+
+(NUD_NAMES, NUD_VALUES) = map_namespace('NUD_', globals())
 
 
 class ndmsg(nlmsg):
