645715b83088f3036bc1895f4514ab844260398c
Peter V. Saveliev <petr.savelyev@ericsson.com>
2016-04-06 15:32:09 +0200

    rtnl: IPRouteRequest supports encap
    Bug-Url: https://github.com/svinota/pyroute2/issues/227

--- pyroute2/netlink/rtnl/req.py
+++ pyroute2/netlink/rtnl/req.py
@@ -1,7 +1,12 @@
 from socket import AF_INET6
+from pyroute2.common import AF_MPLS
 from pyroute2.common import basestring
 from pyroute2.netlink.rtnl.ifinfmsg import ifinfmsg
 from pyroute2.netlink.rtnl.rtmsg import rtmsg
+
+
+encap_types = {'mpls': 1,
+               AF_MPLS: 1}
 
 
 class IPRequest(dict):
@@ -22,6 +27,46 @@ class IPRouteRequest(IPRequest):
     Utility class, that converts human-readable dictionary
     into RTNL route request.
     '''
+
+    def encap_header(self, header):
+        '''
+        Encap header transform. Format samples:
+
+            {'type': 'mpls',
+             'labels': '200/300'}
+
+            {'type': AF_MPLS,
+             'labels': (200, 300)}
+
+            {'type': 'mpls',
+             'labels': 200}
+
+            {'type': AF_MPLS,
+             'labels': [{'bos': 0, 'label': 200, 'ttl': 16},
+                        {'bos': 1, 'label': 300, 'ttl': 16}]}
+        '''
+        if header['type'] in ('mpls', AF_MPLS):
+            ret = []
+            override_bos = True
+            labels = header['labels']
+            if isinstance(labels, basestring):
+                labels = labels.split('/')
+            if not isinstance(labels, (tuple, list, set)):
+                labels = (labels, )
+            for label in labels:
+                if isinstance(label, dict):
+                    # dicts append intact
+                    override_bos = False
+                    ret.append(label)
+                else:
+                    # otherwise construct label dict
+                    if isinstance(label, basestring):
+                        label = int(label)
+                    ret.append({'bos': 0, 'label': label})
+            # the last label becomes bottom-of-stack
+            if override_bos:
+                ret[-1]['bos'] = 1
+            return {'attrs': [['MPLS_IPTUNNEL_DST', ret]]}
 
     def __setitem__(self, key, value):
         # skip virtual IPDB fields
@@ -45,7 +90,16 @@ class IPRouteRequest(IPRequest):
                 else:
                     raise ValueError('wrong destination')
                 dict.__setitem__(self, 'dst', dst)
-                dict.__setitem__(self, 'dst_len', mask)
+                if mask:
+                    dict.__setitem__(self, 'dst_len', mask)
+        elif key == 'encap':
+            if isinstance(value, dict):
+                dict.__setitem__(self, 'encap_type',
+                                 encap_types[value['type']])
+                dict.__setitem__(self, 'encap',
+                                 self.encap_header(value))
+            else:
+                dict.__setitem__(self, 'encap', value)
         elif key == 'metrics':
             ret = {'attrs': []}
             for name in value:
@@ -56,12 +110,21 @@ class IPRouteRequest(IPRequest):
         elif key == 'multipath':
             ret = []
             for v in value:
+                if 'attrs' in v:
+                    ret.append(v)
+                    continue
                 nh = {'attrs': []}
                 for name in ('flag', 'hops', 'ifindex'):
                     nh[name] = v.pop(name, 0)
                 for name in v:
-                    rta = rtmsg.name2nla(name)
-                    nh['attrs'].append([rta, v[name]])
+                    if name == 'encap' and isinstance(v[name], dict):
+                        nh['attrs'].append(['RTA_ENCAP_TYPE',
+                                            encap_types[v[name]['type']]])
+                        nh['attrs'].append(['RTA_ENCAP',
+                                            self.encap_header(v[name])])
+                    else:
+                        rta = rtmsg.name2nla(name)
+                        nh['attrs'].append([rta, v[name]])
                 ret.append(nh)
             if ret:
                 dict.__setitem__(self, 'multipath', ret)
