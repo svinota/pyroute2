4b747baa8325196534bc2182e5af53bd20068589
Peter V. Saveliev <petr.savelyev@ericsson.com>
2016-04-21 10:39:42 +0200

    examples: update taskstats, don't use raw data
    The raw packet data is not provided anymore.

--- examples/taskstats.py
+++ examples/taskstats.py
@@ -3,15 +3,12 @@ Simple taskstats sample.
 '''
 import os
 from pyroute2 import TaskStats
-from pyroute2.common import hexdump
 
 pid = os.getpid()
 ts = TaskStats()
 # bind is required in the case of generic netlink
 ts.bind()
 ret = ts.get_pid_stat(int(pid))[0]
-# raw hex structure to check alignment
-print(hexdump(ret.raw))
 # parsed structure
 print(ret)
 ts.close()
