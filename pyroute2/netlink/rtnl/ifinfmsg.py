fbca395470f63f04b3c5f099cb99c3cd77e36830
Peter V. Saveliev <petr.savelyev@ericsson.com>
2016-04-23 23:36:43 +0200

    ifinfmsg: vlan NLAs updated
    * updated IFLA_VLAN_QOS_MAPPING
    * updated IFLA_VLAN_INGRESS_QOS
    * added IFLA_VLAN_PROTOCOL

--- pyroute2/netlink/rtnl/ifinfmsg.py
+++ pyroute2/netlink/rtnl/ifinfmsg.py
@@ -541,12 +541,21 @@ class ifinfbase(object):
             nla_map = (('IFLA_VLAN_UNSPEC', 'none'),
                        ('IFLA_VLAN_ID', 'uint16'),
                        ('IFLA_VLAN_FLAGS', 'vlan_flags'),
-                       ('IFLA_VLAN_EGRESS_QOS', 'hex'),
-                       ('IFLA_VLAN_INGRESS_QOS', 'hex'))
+                       ('IFLA_VLAN_EGRESS_QOS', 'qos'),
+                       ('IFLA_VLAN_INGRESS_QOS', 'qos'),
+                       ('IFLA_VLAN_PROTOCOL', 'be16'))
 
             class vlan_flags(nla):
                 fields = (('flags', 'I'),
                           ('mask', 'I'))
+
+            class qos(nla):
+                nla_map = (('IFLA_VLAN_QOS_UNSPEC', 'none'),
+                           ('IFLA_VLAN_QOS_MAPPING', 'qos_mapping'))
+
+                class qos_mapping(nla):
+                    fields = (('from', 'I'),
+                              ('to', 'I'))
 
         class bridge_data(nla):
             prefix = 'IFLA_BR_'
