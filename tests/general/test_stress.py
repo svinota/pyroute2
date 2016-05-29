0d54652d4f632f96549f6c08d5a98ea79d573f65
Roman Bolshakov <Roman.Bolshakov@emc.com>
2016-05-29 11:00:13 -0400

    Close sentinel explicitly to avoid pipe leak.

--- tests/general/test_stress.py
+++ tests/general/test_stress.py
@@ -1,6 +1,9 @@
 from pyroute2 import IPRoute
 from pyroute2 import IPDB
 from pyroute2 import NetNS
+from nose.tools import assert_raises
+import fcntl
+import sys
 
 RESPAWNS = 1200
 
@@ -28,8 +31,14 @@ class TestRespawn(object):
 class TestNetNS(object):
 
     def test_fd_leaks(self):
+        namespaces = []
         for i in range(RESPAWNS):
             nsid = 'leak_%i' % i
             ns = NetNS(nsid)
             ns.close()
             ns.remove()
+            namespaces.append(ns)
+        if sys.version_info > (3, 2) and sys.version_info < (3, 6):
+            for n in namespaces:
+                assert_raises(OSError,
+                              fcntl.fcntl, ns.server.sentinel, fcntl.F_GETFD)
