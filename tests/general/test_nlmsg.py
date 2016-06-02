60d4bb7b2c5d148a6f9a7442369b3e6b7756f964
Peter V. Saveliev <petr.savelyev@ericsson.com>
2016-06-02 18:06:29 +0200

    test: check netlink NLA getters

--- tests/general/test_nlmsg.py
+++ tests/general/test_nlmsg.py
@@ -1,6 +1,36 @@
 from pyroute2.common import load_dump
+from pyroute2.netlink import nlmsg
 from pyroute2.netlink.rtnl.iprsocket import MarshalRtnl
 from pyroute2.netlink.nl80211 import MarshalNl80211
+
+
+prime = {'attrs': (('A', 2),
+                   ('A', 3),
+                   ('A', 4),
+                   ('B', {'attrs': (('C', 5),
+                                    ('D', {'attrs': (('E', 6),
+                                                     ('F', 7))}))}))}
+
+
+class TestAttrs(object):
+
+    def setup(self):
+        self.msg = nlmsg()
+        self.msg.setvalue(prime)
+
+    def test_get_attr(self):
+        assert self.msg.get_attr('A') == 2
+        assert self.msg.get_attr('C') is None
+
+    def test_get_attrs(self):
+        assert self.msg.get_attrs('A') == [2, 3, 4]
+        assert self.msg.get_attrs('C') == []
+
+    def test_get_nested(self):
+        assert self.msg.get_nested('B', 'D', 'E') == 6
+        assert self.msg.get_nested('B', 'D', 'F') == 7
+        assert self.msg.get_nested('B', 'D', 'G') is None
+        assert self.msg.get_nested('C', 'D', 'E') is None
 
 
 class TestNL(object):
