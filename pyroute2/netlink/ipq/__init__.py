8feeb257b27f133befcffff0d597ed047c6c2eb4
Mikko Jaakkola <mjaakkola@ncorecommunications.com>
2016-09-13 13:36:00 -0700

    Fix: IPQ used depricated msg.buf.getvalue() and this changes it to use msg.data

--- pyroute2/netlink/ipq/__init__.py
+++ pyroute2/netlink/ipq/__init__.py
@@ -114,7 +114,7 @@ class IPQSocket(NetlinkSocket):
         msg['header']['type'] = IPQM_MODE
         msg['header']['flags'] = NLM_F_REQUEST
         msg.encode()
-        self.sendto(msg.buf.getvalue(), (0, 0))
+        self.sendto(msg.data, (0, 0))
 
     def verdict(self, seq, v):
         '''
