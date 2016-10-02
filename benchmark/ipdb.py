9a3a6da75056c178713a1c0fa739f563b20b51db
Peter V. Saveliev <petr.savelyev@ericsson.com>
2016-10-02 17:07:25 +0200

    benchmark: split `commit()` statements
    In order to see separately `commit()` and other stuff
    in the line profiler, one should not use `with`
    statements, but call `commit()` explicitly.

--- benchmark/ipdb.py
+++ benchmark/ipdb.py
@@ -9,23 +9,25 @@ def main(ifnum):
             try:
                 # create ports
                 for k in range(30):
-                    with ip.create(kind='dummy',
-                                   ifname='test-%ip%i' % (i, k)) as t:
-                        t.up()
-                # create bridge, bring it up and add ports
-                with ip.create(kind='bridge', ifname='test-%i' % i) as t:
-                    for k in range(30):
-                        t.add_port(ip.interfaces['test-%ip%i' % (i,
-                                                                 k)]['index'])
+                    t = ip.create(kind='dummy', ifname='test-%ip%i' % (i, k))
                     t.up()
+                    t.commit()
+                # create bridge, bring it up and add ports
+                t = ip.create(kind='bridge', ifname='test-%i' % i)
+                for k in range(30):
+                    t.add_port(ip.interfaces['test-%ip%i' % (i, k)]['index'])
+                t.up()
+                t.commit()
                 # add a network one ip by one
                 for k in range(2, 254):
-                    with ip.interfaces['test-%i' % i] as t:
-                        t.add_ip('172.16.%i.%i/24' % (i, k))
+                    t = ip.interfaces['test-%i' % i]
+                    t.add_ip('172.16.%i.%i/24' % (i, k))
+                    t.commit()
                 # add a network at once (compare in line-profiler)
-                with ip.interfaces['test-%i' % i] as t:
-                    for k in range(2, 254):
-                        t.add_ip('172.16.%i.%i/24' % (i + ifnum + 1, k))
+                t = ip.interfaces['test-%i' % i]
+                for k in range(2, 254):
+                    t.add_ip('172.16.%i.%i/24' % (i + ifnum + 1, k))
+                t.commit()
             except:
                 pass
 
