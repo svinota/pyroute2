4f07139611cc1fae4e6c442a27c61b805e15787d
Étienne Noss <etienne.noss@wifirst.fr>
2017-03-13 18:51:33 +0100

    tc: add the mirred action

--- pyroute2/netlink/rtnl/tcmsg/common_act.py
+++ pyroute2/netlink/rtnl/tcmsg/common_act.py
@@ -1,10 +1,13 @@
 from pyroute2.netlink.rtnl.tcmsg import act_gact
 from pyroute2.netlink.rtnl.tcmsg import act_bpf
 from pyroute2.netlink.rtnl.tcmsg import act_police
+from pyroute2.netlink.rtnl.tcmsg import act_mirred
 
 plugins = {'gact': act_gact,
            'bpf': act_bpf,
-           'police': act_police}
+           'police': act_police,
+           'mirred': act_mirred,
+           }
 
 
 class nla_plus_tca_act_opt(object):
