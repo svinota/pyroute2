bed597b10bbdfce525333b27520a07ab8d6b4123
Peter V. Saveliev <peter@svinota.eu>
2017-07-30 14:23:28 +0200

    tc: pep8 fix

--- pyroute2/netlink/rtnl/tcmsg/act_vlan.py
+++ pyroute2/netlink/rtnl/tcmsg/act_vlan.py
@@ -8,13 +8,13 @@ v_actions = {'pop': 1,
 
 
 class options(nla):
-    nla_map = (('TCA_VLAN_UNSPEC','none'),
-               ('TCA_VLAN_TM','none'),
-               ('TCA_VLAN_PARMS','tca_vlan_parms'),
-               ('TCA_VLAN_PUSH_VLAN_ID','uint16'),
-               ('TCA_VLAN_PUSH_VLAN_PROTOCOL','uint16'),
-               ('TCA_VLAN_PAD','none'),
-               ('TCA_VLAN_PUSH_VLAN_PRIORITY','uint8'))
+    nla_map = (('TCA_VLAN_UNSPEC', 'none'),
+               ('TCA_VLAN_TM', 'none'),
+               ('TCA_VLAN_PARMS', 'tca_vlan_parms'),
+               ('TCA_VLAN_PUSH_VLAN_ID', 'uint16'),
+               ('TCA_VLAN_PUSH_VLAN_PROTOCOL', 'uint16'),
+               ('TCA_VLAN_PAD', 'none'),
+               ('TCA_VLAN_PUSH_VLAN_PRIORITY', 'uint8'))
 
     class tca_vlan_parms(nla):
         fields = (('index', 'I'),
@@ -22,20 +22,20 @@ class options(nla):
                   ('action', 'i'),
                   ('refcnt', 'i'),
                   ('bindcnt', 'i'),
-                  ('v_action','i'),)
+                  ('v_action', 'i'),)
 
 
 def get_parameters(kwarg):
     ret = {'attrs': []}
-    parms = { 'v_action': v_actions[kwarg['v_action']]}
+    parms = {'v_action': v_actions[kwarg['v_action']]}
     parms['action'] = tc_actions[kwarg.get('action', 'pipe')]
     ret['attrs'].append(['TCA_VLAN_PARMS', parms])
-    #Vlan id compulsory for "push" and "modify"
-    if kwarg['v_action'] in ['push','modify']:
+    # Vlan id compulsory for "push" and "modify"
+    if kwarg['v_action'] in ['push', 'modify']:
         ret['attrs'].append(['TCA_VLAN_PUSH_VLAN_ID', kwarg['id']])
     if 'priority' in kwarg:
         ret['attrs'].append(['TCA_VLAN_PUSH_VLAN_PRIORITY', kwarg['priority']])
-    if kwarg.get('protocol','802.1Q') == '802.1ad':
+    if kwarg.get('protocol', '802.1Q') == '802.1ad':
         ret['attrs'].append(['TCA_VLAN_PUSH_VLAN_PROTOCOL', htons(0x88a8)])
     else:
         ret['attrs'].append(['TCA_VLAN_PUSH_VLAN_PROTOCOL', htons(0x8100)])
