85c0d2e95724fd45d38cedb436f792c8259a628b
Peter V. Saveliev <peter@svinota.eu>
2018-05-30 20:51:04 +0200

    bsd: userspace RTNL -- initial commit

---
+++ pyroute2/bsd/iproute.py
@@ -0,0 +1,132 @@
+import os
+
+from pyroute2.netlink import (NLM_F_REQUEST,
+                              NLM_F_DUMP,
+                              NLM_F_MULTI,
+                              NLMSG_DONE)
+
+from pyroute2.netlink.rtnl import (RTM_NEWLINK,
+                                   RTM_GETLINK,
+                                   RTM_NEWADDR,
+                                   RTM_GETADDR,
+                                   RTM_NEWROUTE,
+                                   RTM_GETROUTE,
+                                   RTM_NEWNEIGH,
+                                   RTM_GETNEIGH)
+
+from pyroute2.bsd.rtmsocket import RTMSocket
+from pyroute2.bsd.util import Ifconfig
+from pyroute2.netlink.rtnl.marshal import MarshalRtnl
+from pyroute2.netlink.rtnl.ifinfmsg import ifinfmsg
+from pyroute2.netlink.rtnl.ifaddrmsg import ifaddrmsg
+from pyroute2.common import AddrPool
+from pyroute2.common import Namespace
+from pyroute2.proxy import NetlinkProxy
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+
+class IPRoute(object):
+    '''
+    '''
+
+    def __init__(self, *argv, **kwarg):
+        self._ifc = Ifconfig()
+        self.marshal = MarshalRtnl()
+        send_ns = Namespace(self, {'addr_pool': AddrPool(0x10000, 0x1ffff),
+                                   'monitor': False})
+        self._sproxy = NetlinkProxy(policy='return', nl=send_ns)
+        self._fd = None
+        self._pfdr, self._pfdw = os.pipe()
+        self._outq = queue.Queue()
+
+    def bind(self, *argv, **kwarg):
+        self._rtm = RTMSocket()
+        self._fd = self._rtm._sock.fileno()
+
+    def fileno(self):
+        return self._pfdr
+
+    def recv(self, bufsize):
+        os.read(self._pfdr, 1)
+        return self._outq.get()
+
+    def getsockopt(self, *argv, **kwarg):
+        return 1024 * 1024
+
+    def sendto_gate(self, msg, addr):
+        #
+        # handle incoming netlink requests
+        #
+        # sendto_gate() receives single RTNL messages as objects
+        #
+        cmd = msg['header']['type']
+        flags = msg['header']['flags']
+        seq = msg['header']['sequence_number']
+
+        # work only on dump requests for now
+        if flags != NLM_F_REQUEST | NLM_F_DUMP:
+            return
+
+        #
+        if cmd == RTM_GETLINK:
+            rtype = RTM_NEWLINK
+            ret = self.get_links()
+        elif cmd == RTM_GETADDR:
+            rtype = RTM_NEWADDR
+            ret = self.get_addr()
+        elif cmd == RTM_GETROUTE:
+            rtype = RTM_NEWROUTE
+            ret = self.get_routes()
+        elif cmd == RTM_GETNEIGH:
+            rtype = RTM_NEWNEIGH
+            ret = self.get_neighbours()
+
+        #
+        # set response type and finalize the message
+        for r in ret:
+            r['header']['type'] = rtype
+            r['header']['flags'] = NLM_F_MULTI
+            r['header']['sequence_number'] = seq
+
+        #
+        r = type(msg)()
+        r['header']['type'] = NLMSG_DONE
+        r['header']['sequence_number'] = seq
+        ret.append(r)
+
+        data = b''
+        for r in ret:
+            r.encode()
+            data += r.data
+        self._outq.put(data)
+        os.write(self._pfdw, b'\0')
+
+    def get_links(self, *argv, **kwarg):
+        ret = []
+        data = self._ifc.run()
+        parsed = self._ifc.parse(data)
+        for name, spec in parsed['links'].items():
+            msg = ifinfmsg().load(spec)
+            del msg['value']
+            ret.append(msg)
+        return ret
+
+    def get_addr(self, *argv, **kwarg):
+        ret = []
+        data = self._ifc.run()
+        parsed = self._ifc.parse(data)
+        for name, specs in parsed['addrs'].items():
+            for spec in specs:
+                msg = ifaddrmsg().load(spec)
+                del msg['value']
+                ret.append(msg)
+        return ret
+
+    def get_neighbours(self, *argv, **kwarg):
+        return []
+
+    def get_routes(self, *argv, **kwarg):
+        return []
---
+++ pyroute2/bsd/util.py
@@ -0,0 +1,103 @@
+'''
+Utility to parse ifconfig, netstat etc.
+
+PF_ROUTE may be effectively used only to get notifications. To fetch
+info from the system we have to use ioctl or external utilities.
+
+Maybe some day it will be ioctl. For now it's ifconfig and netstat.
+'''
+import re
+import socket
+import subprocess
+
+
+class Ifconfig(object):
+
+    match = {'NR': re.compile(r'^\b').match}
+
+    def __init__(self, path=''):
+        self.path = path
+
+    def run(self):
+        '''
+        Run the command and get stdout
+        '''
+        cmd = ['ifconfig', ]
+        stdout = stderr = ''
+        try:
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            (stdout, stderr) = process.communicate()
+        except Exception:
+            process.kill()
+        finally:
+            process.wait()
+        return stdout
+
+    def parse_line(self, line):
+        '''
+        Dumb line parser:
+
+        "key1 value1 key2 value2 something"
+          -> {"key1": "value1", "key2": "value2"}
+        '''
+        ret = {}
+        cursor = 0
+        while cursor < (len(line) - 1):
+            ret[line[cursor]] = line[cursor + 1]
+            cursor += 2
+        return ret
+
+    def parse(self, data):
+        '''
+        Parse ifconfig output into netlink-compatible dicts::
+
+            from pyroute2.netlink.rtnl.ifinfmsg import ifinfmsg
+            from pyroute2.bsd.util import Ifconfig
+
+            def links()
+                ifc = Ifconfig()
+                data = ifc.run()
+                for name, spec in ifc.parse(data)["links"].items():
+                    yield ifinfmsg().load(spec)
+        '''
+        current = None
+        ret = {'links': {},
+               'addrs': {}}
+        idx = 0
+        for line in data.split('\n'):
+            sl = line.split()
+            pl = self.parse_line(sl)
+
+            # first line -- ifname, flags, mtu
+            if self.match['NR'](line):
+                current = sl[0][:-1]
+                idx += 1
+                ret['links'][current] = link = {'index': idx,
+                                                'attrs': []}
+                ret['addrs'][current] = addrs = []
+                link['attrs'].append(['IFLA_IFNAME', current])
+
+                # extract MTU
+                if 'mtu' in pl:
+                    link['attrs'].append(['IFLA_MTU', int(pl['mtu'])])
+
+            elif 'ether' in pl:
+                link['attrs'].append(['IFLA_ADDRESS', pl['ether']])
+
+            elif 'inet' in pl:
+                addr = {'index': idx,
+                        'family': socket.AF_INET,
+                        'prefixlen': bin(int(pl['netmask'], 16)).count('1'),
+                        'attrs': [['IFA_ADDRESS', pl['inet']]]}
+                if 'broadcast' in pl:
+                    addr['attrs'].append(['IFA_BROADCAST', pl['broadcast']])
+                addrs.append(addr)
+            elif 'inet6' in pl:
+                addr = {'index': idx,
+                        'family': socket.AF_INET6,
+                        'prefixlen': int(pl['prefixlen']),
+                        'attrs': [['IFA_ADDRESS', pl['inet6'].split('%')[0]]]}
+                if 'scopeid' in pl:
+                    addr['scope'] = int(pl['scopeid'], 16)
+                addrs.append(addr)
+        return ret
