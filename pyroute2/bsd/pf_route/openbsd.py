fdf6ad9ca4194b66ceee9bbfb37b437a63cee0d7
Peter V. Saveliev <peter@svinota.eu>
2018-06-01 10:53:58 +0200

    pf_route: fix netmask decoding

--- pyroute2/bsd/pf_route/openbsd.py
+++ pyroute2/bsd/pf_route/openbsd.py
@@ -1,10 +1,6 @@
-import socket
-import struct
-from pyroute2.common import hexdump
 from pyroute2.netlink import nlmsg_base
 
 IFNAMSIZ = 16
-RTAX_MAX = 8
 
 
 class bsdmsg(nlmsg_base):
@@ -19,18 +15,20 @@ class bsdmsg(nlmsg_base):
 class if_msg(bsdmsg):
 
     __slots__ = ()
-    fields = (('ifm_addrs', 'i'),
+    fields = (('ifm_index', 'H'),
+              ('ifm_tableid', 'H'),
+              ('ifm_pad1', 'B'),
+              ('ifm_pad2', 'B'),
+              ('ifm_addrs', 'i'),
               ('ifm_flags', 'i'),
-              ('ifm_index', 'H'),
+              ('ifm_xflags', 'i'),
               ('ifi_type', 'B'),
-              ('ifi_physical', 'B'),
               ('ifi_addrlen', 'B'),
               ('ifi_hdrlen', 'B'),
               ('ifi_link_state', 'B'),
-              ('ifi_vhid', 'B'),
-              ('ifi_datalen', 'H'),
               ('ifi_mtu', 'I'),
               ('ifi_metric', 'I'),
+              ('ifi_rdomain', 'I'),
               ('ifi_baudrate', 'Q'),
               ('ifi_ipackets', 'Q'),
               ('ifi_ierrors', 'Q'),
@@ -44,20 +42,12 @@ class if_msg(bsdmsg):
               ('ifi_iqdrops', 'Q'),
               ('ifi_oqdrops', 'Q'),
               ('ifi_noproto', 'Q'),
-              ('ifi_hwassist', 'Q'),
-              ('ifu_tt', 'Q'),
-              ('ifu_tv1', 'Q'),
-              ('ifu_tv2', 'Q'))
+              ('ifi_capabilities', 'I'),
+              ('ifu_sec', 'Q'),
+              ('ifu_usec', 'I'))
 
 
-class rt_slot(nlmsg_base):
-
-    __slots__ = ()
-    header = (('length', 'B'),
-              ('family', 'B'))
-
-
-class rt_msg(bsdmsg):
+class rt_msg_base(bsdmsg):
 
     __slots__ = ()
     fields = (('rtm_index', 'H'),
@@ -83,7 +73,7 @@ class rt_msg(bsdmsg):
               ('rmx_rtt', 'I'),
               ('rmx_rttvar', 'I'),
               ('rmx_pad', 'I'))
-    sockaddr_offset = 92
+    sockaddr_offset = 96
 
     ifa_slots = {0: ('DST', 'rt_slot_addr'),
                  1: ('GATEWAY', 'rt_slot_addr'),
@@ -101,82 +91,8 @@ class rt_msg(bsdmsg):
                  13: ('STATIC', 'hex'),
                  14: ('SEARCH', 'hex')}
 
-    class hex(rt_slot):
 
-        def decode(self):
-            rt_slot.decode(self)
-            length = self['header']['length']
-            self['value'] = hexdump(self.data[self.offset + 2:
-                                              self.offset + length])
-
-    class rt_slot_ifp(rt_slot):
-
-        def decode(self):
-            rt_slot.decode(self)
-            #
-            # Structure
-            #     0       1       2       3       4       5       6       7
-            # |-------+-------+-------+-------|-------+-------+-------+-------|
-            # |  len  |  fam  |    ifindex    |   ?   |  nlen |    padding?   |
-            # |-------+-------+-------+-------|-------+-------+-------+-------|
-            # | ...
-            # | ...
-            #
-            # len -- sockaddr len
-            # fam -- sockaddr family
-            # ifindex -- interface index
-            # ? -- no idea, probably again some sockaddr related info?
-            # nlen -- device name length
-            # padding? -- probably structure alignment
-            #
-            (self['index'], _,
-             name_length) = struct.unpack('HBB', self.data[self.offset + 2:
-                                                           self.offset + 6])
-            self['ifname'] = self.data[self.offset + 8:
-                                       self.offset + 8 + name_length]
-
-    class rt_slot_addr(rt_slot):
-
-        def decode(self):
-            alen = {socket.AF_INET: 4,
-                    socket.AF_INET6: 16}
-            rt_slot.decode(self)
-            #
-            # Yksinkertainen: only the sockaddr family (one byte) and the
-            # network address.
-            #
-            # But for netmask it's completely screwed up. E.g.:
-            #
-            #  ifconfig disc2 10.0.0.1 255.255.255.0 up
-            # -->
-            #  ... NETMASK: 38:12:00:00:ff:00:00:00:00:00:00:...
-            #
-            # Why?!
-            #
-            addrlen = alen.get(self['header']['family'], 0)
-            family = self['header']['family']
-            length = self['header']['length']
-            data = self.data[self.offset + 4:
-                             self.offset + 4 + addrlen]
-            if family in (socket.AF_INET, socket.AF_INET6):
-                self['address'] = socket.inet_ntop(family, data)
-            else:
-                self['raw'] = self.data[self.offset:
-                                        self.offset + length]
-
-    def decode(self):
-        bsdmsg.decode(self)
-        offset = self.sockaddr_offset
-        for i in range(RTAX_MAX):
-            if self['rtm_addrs'] & (1 << i):
-                handler = getattr(self, self.ifa_slots[i][1])
-                slot = handler(self.data[offset:])
-                slot.decode()
-                offset += slot['header']['length']
-                self[self.ifa_slots[i][0]] = slot
-
-
-class ifa_msg(rt_msg):
+class ifa_msg_base(bsdmsg):
 
     __slots__ = ()
     fields = (('ifam_index', 'H'),
@@ -186,10 +102,10 @@ class ifa_msg(rt_msg):
               ('rtm_addrs', 'i'),
               ('ifam_flags', 'i'),
               ('ifam_metric', 'i'))
-    sockaddr_offset = 22
+    sockaddr_offset = 24
 
 
-class ifma_msg(rt_msg):
+class ifma_msg_base(bsdmsg):
 
     __slots__ = ()
     fields = (('rtm_addrs', 'i'),
