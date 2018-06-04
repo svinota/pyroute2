5341397872c7e432de18216d9a64cc6eb60bc982
Peter V. Saveliev <peter@svinota.eu>
2018-06-04 10:07:29 +0200

    ndb: SQL schema improvements
    — Foreign keys
    — MP routes: separate NH table

--- pyroute2/ndb/dbschema.py
+++ pyroute2/ndb/dbschema.py
@@ -1,3 +1,4 @@
+import uuid
 import threading
 from functools import partial
 from collections import OrderedDict
@@ -5,6 +6,7 @@ from pyroute2.netlink.rtnl.ifinfmsg import ifinfmsg
 from pyroute2.netlink.rtnl.ifaddrmsg import ifaddrmsg
 from pyroute2.netlink.rtnl.ndmsg import ndmsg
 from pyroute2.netlink.rtnl.rtmsg import rtmsg
+from pyroute2.netlink.rtnl.rtmsg import nh
 
 
 class DBSchema(object):
@@ -15,7 +17,8 @@ class DBSchema(object):
     schema = {'interfaces': OrderedDict(ifinfmsg.sql_schema()),
               'addresses': OrderedDict(ifaddrmsg.sql_schema()),
               'neighbours': OrderedDict(ndmsg.sql_schema()),
-              'routes': OrderedDict(rtmsg.sql_schema())}
+              'routes': OrderedDict(rtmsg.sql_schema()),
+              'nh': OrderedDict(nh.sql_schema())}
     classes = {'interfaces': ifinfmsg,
                'addresses': ifaddrmsg,
                'neighbours': ndmsg,
@@ -31,15 +34,25 @@ class DBSchema(object):
                         'tos',
                         'RTA_TABLE',
                         'RTA_DST',
-                        'RTA_PRIORITY')}
+                        'RTA_PRIORITY'),
+             'nh': ('route_id',
+                    'nh_id')}
+
+    foreign_key = {'addresses': [('(f_index)', 'interfaces(f_index)'), ],
+                   'neighbours': [('(f_ifindex)', 'interfaces(f_index)'), ],
+                   'routes': [('(f_RTA_OIF)', 'interfaces(f_index)'),
+                              ('(f_RTA_IIF)', 'interfaces(f_index)')],
+                   'nh': [('(f_route_id)', 'routes(f_route_id)'), ]}
 
     def __init__(self, db, tid):
         self.thread = tid
         self.db = db
+        self.db.execute('PRAGMA foreign_keys = ON')
         for table in ('interfaces',
                       'addresses',
                       'neighbours',
-                      'routes'):
+                      'routes',
+                      'nh'):
             self.create_table(table)
 
     def create_table(self, table):
@@ -51,6 +64,11 @@ class DBSchema(object):
             # names may not be used in SQL statements
             #
             req.append('f_%s %s' % field)
+        if table in self.foreign_key:
+            for key in self.foreign_key[table]:
+                req.append('FOREIGN KEY %s REFERENCES %s '
+                           'ON UPDATE CASCADE '
+                           'ON DELETE CASCADE ' % key)
         req = ','.join(req)
         req = ('CREATE TABLE IF NOT EXISTS '
                '%s (%s)' % (table, req))
@@ -80,6 +98,52 @@ class DBSchema(object):
             ret.append(dict(zip(self.schema[table].keys(), record)))
         return ret
 
+    def load_rtmsg(self, target, event):
+        mp = event.get_attr('RTA_MULTIPATH')
+
+        if (not event['header']['type'] % 2) and mp:
+            # create & mp route
+            #
+            # create key
+            keys = ['target = ?']
+            values = [target]
+            for key in self.index['routes']:
+                keys.append('f_%s = ?' % key)
+                values.append(event.get(key) or event.get_attr(key))
+            #
+            spec = 'WHERE %s' % ' AND '.join(keys)
+            s_req = 'SELECT f_route_id FROM routes %s' % spec
+            #
+            # get existing route_id
+            route_id = self.db.execute(s_req, values).fetchall()
+            if route_id:
+                #
+                # if exists
+                route_id = route_id[0][0]
+                #
+                # flush all previous MP hops
+                d_req = 'DELETE FROM nh WHERE f_route_id= ?'
+                self.db.execute(d_req, (route_id, ))
+            else:
+                #
+                # or create a new route_id
+                route_id = str(uuid.uuid4())
+            #
+            # set route_id on the route itself
+            event['route_id'] = route_id
+            self.load_netlink('routes', target, event)
+            for idx in range(len(mp)):
+                mp[idx]['header'] = {}          # for load_netlink()
+                mp[idx]['route_id'] = route_id  # set route_id on NH
+                mp[idx]['nh_id'] = idx          # add NH number
+                self.load_netlink('nh', target, mp[idx])
+            #
+            # we're done with an MP-route, just exit
+            return
+        #
+        # ... or work on a regular route
+        self.load_netlink("routes", target, event)
+
     def load_netlink(self, table, target, event):
         #
         # Simple barrier to work with the DB only from
@@ -91,7 +155,7 @@ class DBSchema(object):
         #
         # The event type
         #
-        if event['header']['type'] % 2:
+        if event['header'].get('type', 0) % 2:
             #
             # Delete an object
             #
@@ -112,16 +176,15 @@ class DBSchema(object):
             values = [target]
             for field in fkeys:
                 value = event.get_attr(field) or event.get(field)
-                if field in self.index[table] and value is None:
-                    if self.schema[table][field] == 'INTEGER':
-                        value = 0
-                    else:
-                        value = ''
-
                 values.append(value)
-
-            self.db.execute('INSERT OR REPLACE INTO %s (%s)'
-                            ' VALUES (%s)' % (table, fields, pch), values)
+            try:
+                self.db.execute('INSERT OR REPLACE INTO %s (%s)'
+                                ' VALUES (%s)' % (table, fields, pch), values)
+            except Exception:
+                #
+                # A good question, what should we do here
+                import traceback
+                traceback.print_exc()
 
 
 def init(db, tid):
@@ -129,5 +192,5 @@ def init(db, tid):
     ret.event_map = {ifinfmsg: partial(ret.load_netlink, 'interfaces'),
                      ifaddrmsg: partial(ret.load_netlink, 'addresses'),
                      ndmsg: partial(ret.load_netlink, 'neighbours'),
-                     rtmsg: partial(ret.load_netlink, 'routes')}
+                     rtmsg: ret.load_rtmsg}
     return ret
