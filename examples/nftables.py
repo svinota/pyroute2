52202d5a55bbd0dfde090a3c06ec49ea3aaa6356
Cooper Ry Lees <me@cooperlees.com>
2020-11-17 05:58:45 +0000

    Add nftables docs + example - Add nftables docs to be generated - Add a very simple example showing pulling from inet table

---
+++ examples/nftables.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from pyroute2.nftables.main import NFTables
+
+
+# nfgen_family 0 == inet
+def show_nftables(family: int = 0) -> None:
+        nft = NFTables(nfgen_family=family)
+        tables = nft.get_tables()
+        chains = nft.get_chains()
+        rules = nft.get_rules()
+
+        print("Tables:")
+        print(tables)
+        print("\nChains:")
+        print(chains)
+        print("\nRules:")
+        for rule in rules:
+            print(rule, type(rule))
+        print(f"Interfaces:\n{avail_interfaces}")
+
+
+show_nftables(0)
