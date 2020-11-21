0bac5d55a862e7dd07c42a190b54e9d10c3fa0ea
Peter V. Saveliev <peter@svinota.eu>
2020-11-21 11:04:52 +0000

    examples: pep8 fix

--- examples/nftables.py
+++ examples/nftables.py
@@ -5,18 +5,18 @@ from pyroute2.nftables.main import NFTables
 
 # nfgen_family 0 == inet
 def show_nftables(family: int = 0) -> None:
-        nft = NFTables(nfgen_family=family)
-        tables = nft.get_tables()
-        chains = nft.get_chains()
-        rules = nft.get_rules()
+    nft = NFTables(nfgen_family=family)
+    tables = nft.get_tables()
+    chains = nft.get_chains()
+    rules = nft.get_rules()
 
-        print("Tables:")
-        print(tables)
-        print("\nChains:")
-        print(chains)
-        print("\nRules:")
-        for rule in rules:
-            print(rule, type(rule))
+    print("Tables:")
+    print(tables)
+    print("\nChains:")
+    print(chains)
+    print("\nRules:")
+    for rule in rules:
+        print(rule, type(rule))
 
 
 show_nftables(0)
