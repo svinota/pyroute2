ac675f0e75aa260ad6e359faad8f1a0acb241a38
Peter V. Saveliev <peter@svinota.eu>
2021-03-23 18:59:05 +0100

    ndb: global transactions prototype + test

--- pyroute2/ndb/main.py
+++ pyroute2/ndb/main.py
@@ -223,6 +223,65 @@ class PostgreSQLAdapter(object):
         return "'%s'" % json.dumps(self.obj)
 
 
+class Transaction(object):
+
+    def __init__(self, log):
+        self.queue = []
+        self.event = threading.Event()
+        self.event.clear()
+        self.log = log.channel('transaction.%s' % id(self))
+        self.log.debug('begin transaction')
+
+    def push(self, *argv):
+        for obj in argv:
+            self.log.debug('queue %s' % type(obj))
+            self.queue.append(obj)
+        return self
+
+    def append(self, obj):
+        self.log.debug('queue %s' % type(obj))
+        self.push(obj)
+        return self
+
+    def pop(self, index=-1):
+        self.log.debug('pop %s' % index)
+        self.queue.pop(index)
+        return self
+
+    def insert(self, index, obj):
+        self.log.debug('insert %i %s' % (index, type(obj)))
+        self.queue.insert(index, obj)
+        return self
+
+    def cancel(self):
+        self.log.debug('cancel transaction')
+        self.queue = []
+        return self
+
+    def wait(self):
+        return self.event.wait()
+
+    def done(self):
+        return self.event.is_set()
+
+    def commit(self):
+        self.log.debug('commit')
+        rollbacks = []
+        for obj in self.queue:
+            rollbacks.append(obj)
+            try:
+                obj.commit()
+            except Exception:
+                for rb in reversed(rollbacks):
+                    try:
+                        rb.rollback()
+                    except Exception as e:
+                        self.log.warning(f'ignore rollback exception: {e}')
+                raise
+        self.event.set()
+        return self
+
+
 class View(dict):
     '''
     The View() object returns RTNL objects on demand::
@@ -881,6 +940,9 @@ class NDB(object):
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
+
+    def begin(self):
+        return Transaction(self.log)
 
     def auth_proxy(self, auth_manager):
         return AuthProxy(self, [auth_manager, ])
