62ec195f8a4e070cdebd36e88774dfcc5099ec3b
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2021-11-17 09:51:02 +0100

    rtnl: add documentation and examples for SRv6 End.DT4 behavior

--- pyroute2.core/pr2modules/netlink/rtnl/req.py
+++ pyroute2.core/pr2modules/netlink/rtnl/req.py
@@ -222,6 +222,10 @@ class IPRouteRequest(IPRequest):
              'table': '10'}
 
             {'type': 'seg6local',
+             'action': 'End.DT4',
+             'vrf_table': 10}
+
+            {'type': 'seg6local',
              'action': 'End.B6',
              'table': '10'
              'srh': {'segs': '2000::5,2000::6'}}
