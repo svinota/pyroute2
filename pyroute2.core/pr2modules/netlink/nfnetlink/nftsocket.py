32163fd157c5c4d72bd8f952749d9a231f68cae1
B. Blechschmidt <github@blechschmidt.pw>
2022-04-13 22:18:48 +0200

    Fix crash when decoding nftables rules with ranges

--- pyroute2.core/pr2modules/netlink/nfnetlink/nftsocket.py
+++ pyroute2.core/pr2modules/netlink/nfnetlink/nftsocket.py
@@ -653,7 +653,7 @@ class nft_contains_expr:
             class quota_flags(nft_flags_be32):
                 ops = ('NFT_QUOTA_F_INV', 'NFT_QUOTA_F_DEPLETED')
 
-        class nft_range(nft_regs):
+        class nft_range(nft_regs, nft_data):
             nla_map = (
                 ('NFTA_RANGE_UNSPEC', 'none'),
                 ('NFTA_RANGE_SREG', 'regs'),
