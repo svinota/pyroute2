5ff8fe8cff3b4fdce7be2e404523b2c2817f0292
Peter Saveliev <peter@svinota.eu>
2022-06-22 20:33:17 +0200

    ci: fix code formatting

--- tests/test_linux/pr2test/marks.py
+++ tests/test_linux/pr2test/marks.py
@@ -2,5 +2,8 @@ import getpass
 
 import pytest
 
+
 def require_root():
-    return pytest.mark.skipif(getpass.getuser() != 'root', reason='no root access')
+    return pytest.mark.skipif(
+        getpass.getuser() != 'root', reason='no root access'
+    )
