01e09474340d3e96b8689c8d72f2af7b36d974cd
Peter Saveliev <peter@svinota.eu>
2022-07-09 14:33:56 +0200

    ci/unit: add dump filters for link requests

--- tests/test_unit/test_requests/test_link.py
+++ tests/test_unit/test_requests/test_link.py
@@ -3,15 +3,20 @@ from common import Request, Result, run_test
 
 from pr2modules.requests.link import LinkFieldFilter, LinkIPRouteFilter
 
-config = {
+config_add = {
     'filters': (
         {'class': LinkFieldFilter, 'argv': []},
         {'class': LinkIPRouteFilter, 'argv': ['add']},
     )
 }
+config_dump = {
+    'filters': (
+        {'class': LinkFieldFilter, 'argv': []},
+        {'class': LinkIPRouteFilter, 'argv': ['dump']},
+    )
+}
 
-
-result = Result(
+result_add = Result(
     {
         'index': 1,
         'change': 0,
@@ -19,16 +24,68 @@ result = Result(
         'IFLA_LINKINFO': {'attrs': [['IFLA_INFO_KIND', 'dummy']]},
     }
 )
+result_dump = Result({'index': 1, ('linkinfo', 'kind'): 'dummy'})
+
+
+@pytest.mark.parametrize(
+    'config,spec,result',
+    (
+        (config_add, Request({'index': 1, 'kind': 'dummy'}), result_add),
+        (config_add, Request({'index': [1], 'kind': 'dummy'}), result_add),
+        (config_add, Request({'index': (1,), 'kind': 'dummy'}), result_add),
+        (config_dump, Request({'index': 1, 'kind': 'dummy'}), result_dump),
+        (config_dump, Request({'index': [1], 'kind': 'dummy'}), result_dump),
+        (config_dump, Request({'index': (1,), 'kind': 'dummy'}), result_dump),
+    ),
+    ids=[
+        'int-add',
+        'list-add',
+        'tuple-add',
+        'int-dump',
+        'list-dump',
+        'tuple-dump',
+    ],
+)
+def test_index(config, spec, result):
+    return run_test(config, spec, result)
 
 
 @pytest.mark.parametrize(
     'spec,result',
     (
-        (Request({'index': 1, 'kind': 'dummy'}), result),
-        (Request({'index': [1], 'kind': 'dummy'}), result),
-        (Request({'index': (1,), 'kind': 'dummy'}), result),
+        (
+            Request({'kind': 'bridge', 'br_stp_state': 1}),
+            Result(
+                {
+                    ('linkinfo', 'kind'): 'bridge',
+                    ('linkinfo', 'data', 'br_stp_state'): 1,
+                }
+            ),
+        ),
+        (
+            Request({'kind': 'bond', 'bond_primary': 1}),
+            Result(
+                {
+                    ('linkinfo', 'kind'): 'bond',
+                    ('linkinfo', 'data', 'bond_primary'): 1,
+                }
+            ),
+        ),
+        (
+            Request({'kind': 'vxlan', 'vxlan_id': 1}),
+            Result(
+                {
+                    ('linkinfo', 'kind'): 'vxlan',
+                    ('linkinfo', 'data', 'vxlan_id'): 1,
+                }
+            ),
+        ),
+        (
+            Request({'kind': 'fake', 'fake_attr': 1}),
+            Result({('linkinfo', 'kind'): 'fake', 'fake_attr': 1}),
+        ),
     ),
-    ids=['int', 'list', 'tuple'],
+    ids=['bridge', 'bond', 'vxlan', 'fake'],
 )
-def test_index(spec, result):
-    return run_test(config, spec, result)
+def test_dump_specific(spec, result):
+    return run_test(config_dump, spec, result)
