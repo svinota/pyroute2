9e8837e67cca3acd2ec6026774c16c425fe5e10c
Peter Saveliev <peter@svinota.eu>
2022-07-16 23:57:15 +0200

    ci: provide more info in test_lab assertions

--- examples/lab/example1/check.py
+++ examples/lab/example1/check.py
@@ -1,8 +1,10 @@
 from setup import lab
 from task import IPRoute
 
-assert len(lab.registry) == 1
-assert isinstance(lab.registry[0], IPRoute)
+if len(lab.registry) != 1:
+    raise AssertionError('expected exactly one IPRoute instance')
+if not isinstance(lab.registry[0], IPRoute):
+    raise AssertionError('expected IPRoute instance')
 
 ipr = lab.registry[0]
 ipr.get_links.assert_called()
