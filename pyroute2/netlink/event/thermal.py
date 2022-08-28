5045cffa4249de27c62ddd5a31f8e92a36b54be7
Peter Saveliev <peter@svinota.eu>
2022-08-28 14:17:19 +0200

    thermal: update NLA list
    Bug-Url: https://github.com/svinota/pyroute2/issues/1002

--- pyroute2/netlink/event/thermal.py
+++ pyroute2/netlink/event/thermal.py
@@ -12,8 +12,30 @@ THERMAL_GENL_CMD_EVENT = 1
 class thermal_msg(genlmsg):
     nla_map = (
         ('THERMAL_GENL_ATTR_UNSPEC', 'none'),
-        ('THERMAL_GENL_ATTR_EVENT', 'hex'),
-    )  # to be done
+        ('THERMAL_GENL_ATTR_TZ', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_ID', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_TEMP', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_TRIP', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_TRIP_ID', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_TRIP_TYPE', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_TRIP_TEMP', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_TRIP_HYST', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_MODE', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_NAME', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_CDEV_WEIGHT', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_GOV', 'hex'),
+        ('THERMAL_GENL_ATTR_TZ_GOV_NAME', 'hex'),
+        ('THERMAL_GENL_ATTR_CDEV', 'hex'),
+        ('THERMAL_GENL_ATTR_CDEV_ID', 'hex'),
+        ('THERMAL_GENL_ATTR_CDEV_CUR_STATE', 'hex'),
+        ('THERMAL_GENL_ATTR_CDEV_MAX_STATE', 'hex'),
+        ('THERMAL_GENL_ATTR_CDEV_NAME', 'hex'),
+        ('THERMAL_GENL_ATTR_GOV_NAME', 'hex'),
+        ('THERMAL_GENL_ATTR_CPU_CAPABILITY', 'hex'),
+        ('THERMAL_GENL_ATTR_CPU_CAPABILITY_ID', 'hex'),
+        ('THERMAL_GENL_ATTR_CPU_CAPABILITY_PERFORMANCE', 'hex'),
+        ('THERMAL_GENL_ATTR_CPU_CAPABILITY_EFFICIENCY', 'hex'),
+    )
 
 
 class MarshalThermalEvent(Marshal):
