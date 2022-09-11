f76102c34bc5ea1742094b6b9614f2ce4496688d
Peter Saveliev <peter@svinota.eu>
2022-09-11 22:45:06 +0200

    nlmsg: rename tests

--- tests/test_linux/test_nlmsg/test_nlmsg.py
+++ tests/test_linux/test_nlmsg/test_nlmsg.py
@@ -3,7 +3,7 @@ from pr2test.marks import require_root
 pytestmark = [require_root()]
 
 
-def test_nla_operators(context):
+def test_nlmsg_operators(context):
     ifname = context.new_ifname
     ipaddr1 = context.new_ipaddr
     ipaddr2 = context.new_ipaddr
@@ -29,19 +29,21 @@ def test_nla_operators(context):
     assert intersection['index'] == context.ndb.interfaces[ifname]['index']
 
 
-def test_nla_compare_equal(context):
+def test_nlmsg_compare_equal(context):
     lvalue = tuple(context.ipr.get_links())[0]
     rvalue = tuple(context.ipr.get_links())[0]
     assert lvalue is not rvalue
     assert lvalue == rvalue
 
-def test_nla_compare_not_equal(context):
+
+def test_nlmsg_compare_not_equal(context):
     lvalue = tuple(context.ipr.get_links())[0]
     rvalue = tuple(context.ipr.get_links())[1]
     assert lvalue is not rvalue
     assert lvalue != rvalue
 
-def test_nla_compare_int(context):
+
+def test_nlmsg_compare_int(context):
     lvalue = tuple(context.ipr.get_links())[0]
     rvalue = 42
     assert lvalue is not rvalue
