71dea1a8ed7d715e651c9a4e4594c42e20ea4214
Peter Saveliev <peter@svinota.eu>
2022-10-04 15:19:11 +0200

    connector.cn_proc: events structures

--- pyroute2/netlink/connector/cn_proc.py
+++ pyroute2/netlink/connector/cn_proc.py
@@ -1,3 +1,4 @@
+from pyroute2.common import map_namespace
 from pyroute2.netlink import NLMSG_DONE
 from pyroute2.netlink.nlsocket import Marshal
 
@@ -16,6 +17,8 @@ PROC_EVENT_COMM = 0x200
 PROC_EVENT_COREDUMP = 0x40000000
 PROC_EVENT_EXIT = 0x80000000
 
+(PROC_BY_NAMES, PROC_BY_IDS) = map_namespace('PROC_', globals())
+
 CN_IDX_PROC = 0x1
 CN_VAL_PROC = 0x1
 
@@ -24,16 +27,100 @@ PROC_CN_MCAST_IGNORE = 0x2
 
 
 class proc_event_base(cn_msg):
-
     fields = cn_msg.fields + (
         ('what', 'I'),
         ('cpu', 'I'),
         ('timestamp_ns', 'Q'),
     )
 
+    def decode(self):
+        super().decode()
+        self['event'] = PROC_BY_IDS.get(self['what'], 'UNDEFINED')
+
+
+class proc_event_fork(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('parent_pid', 'I'),
+        ('parent_tgid', 'I'),
+        ('child_pid', 'I'),
+        ('child_tgid', 'I'),
+    )
+
+
+class proc_event_exec(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+    )
+
+
+class proc_event_uid(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+        ('ruid', 'I'),
+        ('rgid', 'I'),
+    )
+
+
+class proc_event_gid(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+        ('euid', 'I'),
+        ('egid', 'I'),
+    )
+
+
+class proc_event_sid(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+    )
+
+
+class proc_event_ptrace(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+        ('tracer_pid', 'I'),
+        ('tracer_tgid', 'I'),
+    )
+
+
+class proc_event_comm(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+        ('comm', '16s'),
+    )
+
+    def decode(self):
+        super().decode()
+        self['comm'] = self['comm'].decode('utf-8').strip('\x00')
+
+
+class proc_event_coredump(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+        ('parent_pid', 'I'),
+        ('parent_tgid', 'I'),
+    )
+
+
+class proc_event_exit(proc_event_base):
+    fields = proc_event_base.fields + (
+        ('process_pid', 'I'),
+        ('process_tgid', 'I'),
+        ('exit_code', 'I'),
+        ('exit_signal', 'I'),
+        ('parent_pid', 'I'),
+        ('parent_tgid', 'I'),
+    )
+
 
 class proc_event_control(cn_msg):
-
     fields = cn_msg.fields + (('action', 'I'),)
 
 
@@ -41,17 +128,18 @@ class ProcEventMarshal(Marshal):
 
     key_format = 'I'
     key_offset = 36
+    error_type = -1
     msg_map = {
         PROC_EVENT_NONE: proc_event_base,
-        PROC_EVENT_FORK: proc_event_base,
-        PROC_EVENT_EXEC: proc_event_base,
-        PROC_EVENT_UID: proc_event_base,
-        PROC_EVENT_GID: proc_event_base,
-        PROC_EVENT_SID: proc_event_base,
-        PROC_EVENT_PTRACE: proc_event_base,
-        PROC_EVENT_COMM: proc_event_base,
-        PROC_EVENT_COREDUMP: proc_event_base,
-        PROC_EVENT_EXIT: proc_event_base,
+        PROC_EVENT_FORK: proc_event_fork,
+        PROC_EVENT_EXEC: proc_event_exec,
+        PROC_EVENT_UID: proc_event_uid,
+        PROC_EVENT_GID: proc_event_gid,
+        PROC_EVENT_SID: proc_event_sid,
+        PROC_EVENT_PTRACE: proc_event_ptrace,
+        PROC_EVENT_COMM: proc_event_comm,
+        PROC_EVENT_COREDUMP: proc_event_coredump,
+        PROC_EVENT_EXIT: proc_event_exit,
     }
 
 
