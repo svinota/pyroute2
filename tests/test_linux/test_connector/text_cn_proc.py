55a3b02ed146f69a843f13dc0fd3aa9677316893
Peter Saveliev <peter@svinota.eu>
2022-10-05 22:31:47 +0200

    connector.cn_proc: additional tests

--- tests/test_linux/test_connector/text_cn_proc.py
+++ tests/test_linux/test_connector/text_cn_proc.py
@@ -1,11 +1,18 @@
 import os
+import signal
 import subprocess
 
 import pytest
 from pr2test.marks import require_root
 
 from pyroute2 import ProcEventSocket
-from pyroute2.netlink.connector.cn_proc import PROC_EVENT_EXIT, PROC_EVENT_FORK
+from pyroute2.netlink.connector.cn_proc import (
+    PROC_EVENT_EXEC,
+    PROC_EVENT_EXIT,
+    PROC_EVENT_FORK,
+)
+
+pytestmark = [require_root()]
 
 
 class ProcContext:
@@ -17,16 +24,28 @@ class ProcContext:
         self.child.wait()
         self.events = []
         for _ in range(1000):
-            for event in self.ps.get():
-                self.push(event)
-                if (
-                    event['what'] == PROC_EVENT_EXIT
-                    and event['process_pid'] == self.child.pid
-                ):
-                    return
+            if self.match(
+                what=PROC_EVENT_EXIT,
+                process_pid=self.child.pid,
+                source=self.ps.get(),
+            ):
+                return
 
     def push(self, event):
         self.events.append(event)
+
+    def match(self, what, source=None, **kwarg):
+        if source:
+            source = tuple(source)
+            self.events.extend(source)
+        for event in source or self.events:
+            if event['what'] == what:
+                for key, value in kwarg.items():
+                    if event[key] != value:
+                        break
+                else:
+                    return True
+        return False
 
 
 @pytest.fixture
@@ -35,14 +54,25 @@ def cn_proc_context():
         yield ProcContext(ps)
 
 
-@require_root
 def test_event_fork(cn_proc_context):
-    for event in cn_proc_context.events:
-        if (
-            event['what'] == PROC_EVENT_FORK
-            and event['parent_pid'] == os.getpid()
-            and event['child_pid'] == cn_proc_context.child.pid
-        ):
-            break
-    else:
-        raise Exception('expected event not received')
+    assert cn_proc_context.match(
+        what=PROC_EVENT_FORK,
+        parent_pid=os.getpid(),
+        child_pid=cn_proc_context.child.pid,
+    )
+
+
+def test_event_exec(cn_proc_context):
+    assert cn_proc_context.match(
+        what=PROC_EVENT_EXEC, process_pid=cn_proc_context.child.pid
+    )
+
+
+def test_event_exit(cn_proc_context):
+    assert cn_proc_context.match(
+        what=PROC_EVENT_EXIT,
+        process_pid=cn_proc_context.child.pid,
+        exit_code=0,
+        exit_signal=signal.SIGCHLD,
+        parent_pid=os.getpid(),
+    )
