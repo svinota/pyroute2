59d57268a05f13042f400d8d69b80bb0704d991e
Peter Saveliev <peter@svinota.eu>
2024-03-08 20:42:04 +0100

    ipvs: service struct

--- pyroute2/netlink/generic/ipvs.py
+++ pyroute2/netlink/generic/ipvs.py
@@ -1,4 +1,4 @@
-from pyroute2.netlink import genlmsg
+from pyroute2.netlink import genlmsg, nla
 from pyroute2.netlink.generic import GenericNetlinkSocket
 
 GENL_NAME = "IPVS"
@@ -34,13 +34,62 @@ class ipvsmsg(genlmsg):
     prefix = "IPVS_CMD_ATTR_"
     nla_map = (
         ("IPVS_CMD_ATTR_UNSPEC", "none"),
-        ("IPVS_CMD_ATTR_SERVICE", "hex"),
+        ("IPVS_CMD_ATTR_SERVICE", "service"),
         ("IPVS_CMD_ATTR_DEST", "hex"),
         ("IPVS_CMD_ATTR_DAEMON", "hex"),
         ("IPVS_CMD_ATTR_TIMEOUT_TCP", "hex"),
         ("IPVS_CMD_ATTR_TIMEOUT_TCP_FIN", "hex"),
         ("IPVS_CMD_ATTR_TIMEOUT_UDP", "hex"),
     )
+
+    class service(nla):
+        nla_map = (
+            ("IPVS_SVC_ATTR_UNSPEC", "none"),
+            ("IPVS_SVC_ATTR_AF", "uint16"),
+            ("IPVS_SVC_ATTR_PROTOCOL", "uint16"),
+            ("IPVS_SVC_ATTR_ADDR", "target"),
+            ("IPVS_SVC_ATTR_PORT", "be16"),
+            ("IPVS_SVC_ATTR_FWMARK", "uint32"),
+            ("IPVS_SVC_ATTR_SCHED_NAME", "asciiz"),
+            ("IPVS_SVC_ATTR_FLAGS", "hex"),
+            ("IPVS_SVC_ATTR_TIMEOUT", "uint32"),
+            ("IPVS_SVC_ATTR_NETMASK", "ip4addr"),
+            ("IPVS_SVC_ATTR_STATS", "stats"),
+            ("IPVS_SVC_ATTR_PE_NAME", "asciiz"),
+            ("IPVS_SVC_ATTR_STATS64", "stats64"),
+        )
+
+        class stats(nla):
+
+            nla_map = (
+                ("IPVS_STATS_ATTR_UNSPEC", "none"),
+                ("IPVS_STATS_ATTR_CONNS", "uint32"),
+                ("IPVS_STATS_ATTR_INPKTS", "uint32"),
+                ("IPVS_STATS_ATTR_OUTPKTS", "uint32"),
+                ("IPVS_STATS_ATTR_INBYTES", "uint64"),
+                ("IPVS_STATS_ATTR_OUTBYTES", "uint64"),
+                ("IPVS_STATS_ATTR_CPS", "uint32"),
+                ("IPVS_STATS_ATTR_INPPS", "uint32"),
+                ("IPVS_STATS_ATTR_OUTPPS", "uint32"),
+                ("IPVS_STATS_ATTR_INBPS", "uint32"),
+                ("IPVS_STATS_ATTR_OUTBPS", "uint32"),
+            )
+
+        class stats64(nla):
+
+            nla_map = (
+                ("IPVS_STATS_ATTR_UNSPEC", "none"),
+                ("IPVS_STATS_ATTR_CONNS", "uint64"),
+                ("IPVS_STATS_ATTR_INPKTS", "uint64"),
+                ("IPVS_STATS_ATTR_OUTPKTS", "uint64"),
+                ("IPVS_STATS_ATTR_INBYTES", "uint64"),
+                ("IPVS_STATS_ATTR_OUTBYTES", "uint64"),
+                ("IPVS_STATS_ATTR_CPS", "uint64"),
+                ("IPVS_STATS_ATTR_INPPS", "uint64"),
+                ("IPVS_STATS_ATTR_OUTPPS", "uint64"),
+                ("IPVS_STATS_ATTR_INBPS", "uint64"),
+                ("IPVS_STATS_ATTR_OUTBPS", "uint64"),
+            )
 
 
 class IPVSSocket(GenericNetlinkSocket):
