b265592fdbeaba14d3ce8fdd9da34bc4108c1345
Brian Baboch <brian.baboch@wifirst.fr>
2025-01-24 18:03:54 +0100

    dhcp client: implement a hook to add default route

--- pyroute2/dhcp/hooks.py
+++ pyroute2/dhcp/hooks.py
@@ -1,9 +1,11 @@
 '''Hooks called by the DHCP client when bound, a leases expires, etc.'''
 
+import errno
 from logging import getLogger
 
 from pyroute2.dhcp.leases import Lease
 from pyroute2.iproute.linux import AsyncIPRoute
+from pyroute2.netlink.exceptions import NetlinkError
 
 LOG = getLogger(__name__)
 
@@ -47,3 +49,39 @@ class ConfigureIP(Hook):
                 mask=lease.subnet_mask,
                 broadcast=lease.broadcast_address
             )
+
+
+class ConfigureDefaultRoute(Hook):
+    async def bound(self, lease: Lease):
+        if lease.default_gateway is None:
+            LOG.error('Lease does not set the router option')
+            return
+        LOG.info('Adding %s as default route through %s',
+                 lease.default_gateway, lease.interface)
+        async with AsyncIPRoute() as ipr:
+            ifindex = await ipr.link_lookup(ifname=lease.interface),
+            await ipr.route(
+                'replace',
+                dst='0.0.0.0/0',
+                gateway=lease.default_gateway,
+                oif=ifindex
+            )
+
+    async def unbound(self, lease: Lease):
+        if lease.default_gateway is None:
+            LOG.error('Lease does not set the router option')
+            return
+        LOG.info('Removing %s as default route.', lease.default_gateway)
+        async with AsyncIPRoute() as ipr:
+            ifindex = await ipr.link_lookup(ifname=lease.interface)
+            try:
+                await ipr.route(
+                    'del',
+                    dst='0.0.0.0/0',
+                    gateway=lease.default_gateway,
+                    oif=ifindex
+                )
+            except NetlinkError as err:
+                if err.code == errno.ESRCH:
+                    LOG.warning('Default route was already removed by another process')
+                LOG.error('Got a netlink error: %s', err)
