b08a03ae0af3ff4df3257fbf2e40a1a80da9821f
Peter Saveliev <peter@svinota.eu>
2025-02-07 20:29:52 +0100

    fixtures: rename functions to make pylint happy

--- pyroute2/fixtures/iproute.py
+++ pyroute2/fixtures/iproute.py
@@ -1,5 +1,3 @@
-# pylint: disable=redefined-outer-name
-
 import errno
 from collections import namedtuple
 from collections.abc import AsyncGenerator, Generator
@@ -16,8 +14,8 @@ from pyroute2.netlink.exceptions import NetlinkError
 TestInterface = namedtuple('TestInterface', ('index', 'ifname'))
 
 
-@pytest.fixture
-def nsname() -> Generator[str]:
+@pytest.fixture(name='nsname')
+def _nsname() -> Generator[str]:
     '''Create a unique network namespace and yield its name.
 
     Remove the netns on exit.
@@ -34,8 +32,8 @@ def nsname() -> Generator[str]:
         pass
 
 
-@pytest.fixture
-def test_link(nsname: str) -> Generator[TestInterface]:
+@pytest.fixture(name='test_link')
+def _test_link(nsname: str) -> Generator[TestInterface]:
     '''Create a test interface in the test netns and yield TestInterface.
 
     Remove the interface on exit.
@@ -52,20 +50,20 @@ def test_link(nsname: str) -> Generator[TestInterface]:
                 raise
 
 
-@pytest.fixture
-def test_link_index(test_link: TestInterface) -> Generator[int]:
+@pytest.fixture(name='test_link_index')
+def _test_link_index(test_link: TestInterface) -> Generator[int]:
     '''Yield test interface index.'''
     yield test_link.index
 
 
-@pytest.fixture
-def test_link_ifname(test_link: TestInterface) -> Generator[str]:
+@pytest.fixture(name='test_link_ifname')
+def _test_link_ifname(test_link: TestInterface) -> Generator[str]:
     '''Yield test interface name.'''
     yield test_link.ifname
 
 
-@pytest.fixture
-def tmp_link_ifname(nsname: str) -> Generator[str]:
+@pytest.fixture(name='tmp_link_ifname')
+def _tmp_link_ifname(nsname: str) -> Generator[str]:
     '''Yield a temporary interface name.
 
     But don't create it. Try to remove on exit.
@@ -109,29 +107,29 @@ class TestContext:
         return self.ipr.status['netns']
 
 
-@pytest_asyncio.fixture
-async def async_ipr(request, nsname: str) -> AsyncGenerator[AsyncIPRoute]:
+@pytest_asyncio.fixture(name='async_ipr')
+async def _async_ipr(request, nsname: str) -> AsyncGenerator[AsyncIPRoute]:
     kwarg = getattr(request, 'param', {})
     async with AsyncIPRoute(netns=nsname, **kwarg) as ipr:
         yield ipr
 
 
-@pytest.fixture
-def sync_ipr(request, nsname: str) -> Generator[IPRoute]:
+@pytest.fixture(name='sync_ipr')
+def _sync_ipr(request, nsname: str) -> Generator[IPRoute]:
     kwarg = getattr(request, 'param', {})
     with IPRoute(netns=nsname, **kwarg) as ipr:
         yield ipr
 
 
-@pytest_asyncio.fixture
-async def async_context(
+@pytest_asyncio.fixture(name='async_context')
+async def _async_context(
     async_ipr: AsyncIPRoute, test_link: TestInterface
 ) -> AsyncGenerator[TestContext]:
     yield TestContext(async_ipr, test_link)
 
 
-@pytest.fixture
-def sync_context(
+@pytest.fixture(name='sync_context')
+def _sync_context(
     sync_ipr: IPRoute, test_link: TestInterface
 ) -> Generator[TestContext]:
     yield TestContext(sync_ipr, test_link)
