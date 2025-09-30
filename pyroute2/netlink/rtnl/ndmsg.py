svinota/pyroute2

commit 3540ff1619a8bfdc41903a05796780f9b743e4a6

ndmsg: update nda list
Update NDA list.

Link: https://github.com/torvalds/linux/blob/v6.8/net/core/neighbour.c#L1888
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

pyroute2/netlink/rtnl/ndmsg.py
@@ -100,6 +100,9 @@ class ndmsg(nlmsg):
         ('NDA_MASTER', 'uint32'),
         ('NDA_LINK_NETNSID', 'uint32'),
         ('NDA_SRC_VNI', 'uint32'),
+        ('NDA_PROTOCOL', 'uint8'),
+        ('NDA_NH_ID', 'uint32'),
+        ('NDA_FLAGS_EXT', 'uint32'),
     )
 
     class cacheinfo(nla):

commit 192152b50613d720b46b8bcf81f189d9450703c3

ndmsg: fix update nda list
NDA_FDB_EXT_ATTRS was missing. Add two more attributes.

Fixes: 3540ff1619 ("ndmsg: update nda list")
Link: https://github.com/torvalds/linux/blob/v6.14/include/uapi/linux/neighbour.h#L31
Link: https://github.com/svinota/pyroute2/pull/1424#r2367488141
Signed-off-by: Louis Scalbert <louis.scalbert@6wind.com>

pyroute2/netlink/rtnl/ndmsg.py
@@ -102,7 +102,10 @@ class ndmsg(nlmsg):
         ('NDA_SRC_VNI', 'uint32'),
         ('NDA_PROTOCOL', 'uint8'),
         ('NDA_NH_ID', 'uint32'),
+        ('NDA_FDB_EXT_ATTRS', 'nested'),
         ('NDA_FLAGS_EXT', 'uint32'),
+        ('NDA_NDM_STATE_MASK', 'uint16'),
+        ('NDA_NDM_FLAGS_MASK', 'uint8'),
     )
 
     class cacheinfo(nla):