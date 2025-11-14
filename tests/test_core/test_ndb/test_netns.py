3e201ccfdc5dde6bc16850dc135d53873eca38cf
Peter Saveliev <peter@svinota.eu>
2025-11-14 10:38:38 +0100

    ndb: test interfaces to move to netns and back
    Bug-Url: https://github.com/svinota/pyroute2/issues/1442

--- tests/test_core/test_ndb/test_netns.py
+++ tests/test_core/test_ndb/test_netns.py
@@ -3,6 +3,7 @@ from pyroute2.common import uifname
 
 
 def test_intefaces_setns(nsname, ndb):
+    '''Bug-Url: https://github.com/svinota/pyroute2/issues/1439'''
     ifname = uifname()
     with NDB() as ndb_main:
         ndb_main.sources.add(netns=nsname)
@@ -12,6 +13,7 @@ def test_intefaces_setns(nsname, ndb):
 
 
 def test_intefaces_setlocalns(nsname, test_link_ifname, ndb):
+    '''Bug-Url: https://github.com/svinota/pyroute2/issues/1439'''
     with NDB() as ndb_main:
         ndb_main.sources.add(netns=nsname)
         ndb_main.interfaces[
@@ -20,3 +22,66 @@ def test_intefaces_setlocalns(nsname, test_link_ifname, ndb):
     with NDB() as ndb_main:
         assert test_link_ifname in ndb_main.interfaces
         ndb_main.interfaces[test_link_ifname].remove().commit()
+
+
+def test_multiple_there_and_back(nsname, ndb, cleanup):
+    '''Bug-Url: https://github.com/svinota/pyroute2/issues/1442'''
+
+    netns0 = uifname()
+    netns1 = uifname()
+    cleanup.netns.add(netns0)
+    cleanup.netns.add(netns1)
+    ndb.sources.add(netns=netns0)
+    ndb.sources.add(netns=netns1)
+
+    ndb.interfaces.create(ifname="p0", kind="veth", peer="p1").commit()
+    (
+        ndb.interfaces[{"target": "localhost", "ifname": "p0"}]
+        .set("target", netns0)
+        .commit()
+    )
+    (
+        ndb.interfaces[{"target": "localhost", "ifname": "p1"}]
+        .set("target", netns1)
+        .commit()
+    )
+
+    (
+        ndb.interfaces[{"target": netns0, "ifname": "p0"}]
+        .add_ip("172.16.1.1/24")
+        .set('state', 'up')
+        .commit()
+    )
+    (
+        ndb.interfaces[{"target": netns1, "ifname": "p1"}]
+        .add_ip("172.16.1.2/24")
+        .set('state', 'up')
+        .commit()
+    )
+
+    (
+        ndb.interfaces[{"target": netns0, "ifname": "p0"}]
+        .set("state", "down")
+        .del_ip("172.16.1.1/24")
+        .commit()
+    )
+    (
+        ndb.interfaces[{"target": netns1, "ifname": "p1"}]
+        .set("state", "down")
+        .del_ip("172.16.1.2/24")
+        .commit()
+    )
+    (
+        ndb.interfaces[{"target": netns1, "ifname": "p1"}]
+        .set("target", "localhost")
+        .commit()
+    )
+
+    returned = (
+        ndb.interfaces[{"target": netns0, "ifname": "p0"}]
+        .set("target", "localhost")
+        .commit()
+    )
+    lookup = ndb.interfaces[{"ifname": "p0"}]
+    assert returned['target'] == lookup['target']
+    ndb.interfaces[{"target": "localhost", "ifname": "p0"}].remove().commit()
