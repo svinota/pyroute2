f7f92a33ff290f4c8b707098a9ed50d5466db968
Yehonatan Battat <yehonatan.battat@zeronetworks.com>
2025-12-28 11:14:59 +0200

    feat(tc utils): add utils to parse user args

---
+++ pyroute2/netlink/rtnl/tcmsg/utils.py
@@ -0,0 +1,53 @@
+import socket
+
+from pyroute2 import protocols
+
+
+def parse_mac(mac_str):
+    return bytes.fromhex(mac_str.replace(':', ''))
+
+
+def parse_ip(ip_str):
+    addr, _, prefix = ip_str.partition('/')
+
+    if not prefix:
+        return addr, None
+
+    prefix = int(prefix)
+    if _is_ipv6_addr(addr):
+        bits = 128
+        family = socket.AF_INET6
+    else:
+        bits = 32
+        family = socket.AF_INET
+
+    mask = (1 << bits) - (1 << (bits - prefix))
+    mask_bytes = mask.to_bytes(bits // 8, 'big')
+    mask_str = socket.inet_ntop(family, mask_bytes)
+
+    return addr, mask_str
+
+
+def detect_protocol(kwargs):
+    for ip_field in ['src_ip', 'dst_ip']:
+        if ip_field in kwargs:
+            if _is_ipv6_addr(ip_field):
+                return protocols.ETH_P_IPV6
+
+            return protocols.ETH_P_IP
+
+    return protocols.ETH_P_ALL
+
+
+def build_tc_info_field(protocol, prio):
+    return socket.htons(protocol & 0xFFFF) | ((prio << 16) & 0xFFFF0000)
+
+
+def is_ipv6_addr(addr):
+    return ':' in addr
+
+def get_protocol_by_name(protocol_name):
+    try:
+        socket.getprotobyname(protocol_name)
+    except OSError:
+        return None
